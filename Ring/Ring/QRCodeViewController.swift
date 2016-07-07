3b91afff3f278022a440e2704991c30e060c91d6
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-07-07 11:55:49 -0400

    add QRCode Scanner
    - add a qrcode scanner accesible from a right bar button
    on home page
    - add a simple CoreData manager
    - add Contact entity in CoreData
    - When a QRCode is scanned it is added in the db and so appears
    on home screen where we are redirected
    - if the qrcode uri already exist, simply return to home screen
    for now
    - add a little logic to what will be the smartlist on home screen
    
    Change-Id: I297b19a174faafb26224b2337180cb9aac88f272
    Tuleap: #824

---
+++ Ring/Ring/QRCodeViewController.swift
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Edric Ladent-Milaret <edric.ladent-milaret@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import UIKit
+import AVFoundation
+import CoreData
+
+class QRCodeViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    // MARK: - Properties
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+
+    // MARK: - UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input as AVCaptureInput)
+
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+
+            qrCodeFrameView = UIView()
+            qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+            qrCodeFrameView?.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView!)
+            view.bringSubviewToFront(qrCodeFrameView!)
+
+            captureSession?.startRunning()
+
+        } catch {
+            // TODO: Handle that correctly
+            print("Error Opening Capture input")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - AVCaptureMetadataOutputObjectsDelegate
+    func captureOutput(captureOutput: AVCaptureOutput!,
+        didOutputMetadataObjects metadataObjects: [AnyObject]!,
+        fromConnection connection: AVCaptureConnection!) {
+
+            if metadataObjects == nil || metadataObjects.count == 0 {
+                qrCodeFrameView?.frame = CGRectZero
+                return
+            }
+
+            let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+
+            if metadataObj.type == AVMetadataObjectTypeQRCode {
+                let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+                qrCodeFrameView?.frame = barCodeObject.bounds;
+
+                if metadataObj.stringValue != nil {
+                    let predicate = NSPredicate(format: "uri == %@", metadataObj.stringValue)
+                    Table<Contact>.createIfNotExist(predicate) {
+                        $0.uri = metadataObj.stringValue
+                        $0.lastUsed = NSDate().timeIntervalSince1970
+                    }
+                }
+            }
+            self.navigationController?.popViewControllerAnimated(true)
+    }
+}
