52e2e83d84504518cc477d294dc0f15809c4df4a
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2016-12-09 13:31:54 -0500

    account service: modify bridging
    This patch reorganizes the structure of the bridging between the Swift
    client and the Daemon.
    
    It isolates the code related to the account service. That's why there
    is a renaming of the ConfigurationManagerAdaptor.
    We want it to be specialized in the what the account service can do.
    
    The files meant to make the bridge between the client and the daemon
    are now stored in a dedicated file in the project structure.
    
    The broadcast by NSNotification posts is still used here.
    
    Change-Id: I2d8f0d361ed9394c98a30b0703ad3d49b18cde4a

--- Ring/Ring/Bridging/DRingAdaptator.h
+++ Ring/Ring/Bridging/DRingAdaptator.h
@@ -15,21 +15,15 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- * USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
 #import <Foundation/Foundation.h>
 
-#import <map>
-#import <string>
-#import <vector>
-
-@interface Utils : NSObject
-
-+ (NSArray*)vectorToArray:(const std::vector<std::string>&)vector;
-+ (NSMutableDictionary*)mapToDictionnary:
-    (const std::map<std::string, std::string>&)map;
-+ (std::map<std::string, std::string>)dictionnaryToMap:(NSDictionary*)dict;
-
+@interface DRingAdaptator : NSObject
+- (BOOL)initDaemon;
+- (BOOL)startDaemon;
+- (void)fini;
+- (void)pollEvents;
+- (nonnull NSString*)getVersion;
 @end
