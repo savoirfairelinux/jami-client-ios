a929070db640bb34e25d34ea31d7b9cad83f1640
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2016-12-22 12:12:50 -0500

    account service: modify bridging
    This patch reorganizes the structure of the bridging between the Swift
    client and the Daemon.
    
    It isolates the code related to the account service. That's why there
    is a renaming of the ConfigurationManagerAdaptor.
    We want it to be specialized in the what the account service can do.
    
    The files meant to make the bridge between the client and the daemon
    are now stored in a dedicated file in the project structure.
    
    The broadcast by NSNotification posts is still used here.
    
    Tuleap: #1405
    Change-Id: I2d8f0d361ed9394c98a30b0703ad3d49b18cde4a

--- Ring/Ring/Services/AccountsService.swift
+++ Ring/Ring/Services/AccountsService.swift
@@ -21,7 +21,7 @@
 
 class AccountsService {
     // MARK: - Properties
-    fileprivate let confAdapter = ConfigurationManagerAdaptator.sharedManager() as AnyObject
+    fileprivate let confAdapter = AccountAdapter.sharedManager() as AnyObject
 
     /// Fileprivate Accounts list.
     ///
@@ -49,7 +49,7 @@ class AccountsService {
     fileprivate init() {
         accountList = []
 
-        NotificationCenter.default.addObserver(forName: .accountsChanged,
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: kNotificationAccountsChanged),
                                                object: nil,
                                                queue: nil,
                                                using: { _ in
