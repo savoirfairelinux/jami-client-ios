72a1b8d82411f1914be0fd3eb780b84bb6b0412a
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2017-03-07 14:46:48 -0500

    wizard: complete add account pipe
    This patch completes the pipe of creating a dummy account using
    RxSwift and MVVM.
    
    The AccountsService now exposes a public shared responseStream that
    observers can listen to.
    Callers will call this service's methods directly and wait for any
    interesting response on the stream.
    
    Tuleap: #1391
    Change-Id: I2eb5abac9eb012892a806ee2fb73df8730edba87

---
+++ Ring/Ring/Account/CreateRingAccountViewModel.swift
@@ -0,0 +1,84 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import RxSwift
+
+/**
+ A class representing the ViewModel (MVVM) of the accounts managed by Ring.
+ Its responsabilities:
+ - expose to the Views a public API for its interactions concerning the Accounts,
+ - react to the Views user events concerning the Accounts (add an account...)
+ */
+class CreateRingAccountViewModel {
+    /**
+     Dispose bag that contains the Disposable objects of the ViewModel, and managing their disposes.
+     */
+    fileprivate let disposeBag = DisposeBag()
+
+    /**
+     Retains the currently active stream adding an account.
+     Useful to dispose it before starting a new one.
+     */
+    fileprivate var addAccountDisposable: Disposable?
+
+    /**
+     Create the observers to the streams passed in parameters.
+     It will allow this ViewModel to react to other entities' events.
+
+     - Parameter observable: An observable stream to subscribe on.
+     Any observed event on this stream will trigger the action of creating an account.
+     - Parameter onStartCallback: Closure that will be triggered when the action will begin.
+     - Parameter onSuccessCallback: Closure that will be triggered when the action will succeed.
+     - Parameter onErrorCallback: Closure that will be triggered in case of error.
+     */
+    func configureAddAccountObservers(observable: Observable<Void>,
+                                      onStartCallback: ((() -> Void)?),
+                                      onSuccessCallback: ((() -> Void)?),
+                                      onErrorCallback: (((Error?) -> Void)?)) {
+        _ = observable
+            .subscribe(
+                onNext: { [weak self] in
+                    //~ Let the caller know that the action has just begun.
+                    onStartCallback?()
+
+                    //~ Dispose any previously running stream. There is only one add account action
+                    //~ simultaneously authorized.
+                    self?.addAccountDisposable?.dispose()
+                    //~ Subscribe on the AccountsService responseStream to get results.
+                    self?.addAccountDisposable = AccountsService.sharedInstance
+                        .sharedResponseStream
+                        .subscribe(onNext:{ (event) in
+                            if event == AccountRxEvent.AccountChanged {
+                                onSuccessCallback?()
+                            }
+                        }, onError: { error in
+                            onErrorCallback?(error)
+                        })
+                    self?.addAccountDisposable?.addDisposableTo((self?.disposeBag)!)
+
+                    //~ Launch the action.
+                    AccountsService.sharedInstance.addAccount()
+                },
+                onError: { (error) in
+                    onErrorCallback?(error)
+            })
+            .addDisposableTo(disposeBag)
+    }
+}
--- Ring/Ring/Walkthrough/CreateRingAccountViewController.swift
+++ Ring/Ring/Walkthrough/CreateRingAccountViewController.swift
@@ -25,7 +25,7 @@ import RxSwift
 
 class CreateRingAccountViewController: UIViewController {
 
-    var mAccountViewModel: AccountViewModel = AccountViewModel()
+    var mAccountViewModel = CreateRingAccountViewModel()
 
     @IBOutlet weak var mCreateAccountButton: RoundedButton!
 
@@ -48,14 +48,14 @@ class CreateRingAccountViewController: UIViewController {
 
         mAccountViewModel.configureAddAccountObservers(
             observable: createAccountObservable,
-            onStart: { [weak self] in
+            onStartCallback: { [weak self] in
                 self?.setCreateAccountAsLoading()
             },
-            onSuccess: { [weak self] in
+            onSuccessCallback: { [weak self] in
                 print("Account created.")
                 self?.setCreateAccountAsIdle()
             },
-            onError:  { [weak self] (error) in
+            onErrorCallback:  { [weak self] (error) in
                 print("Error creating account...")
                 if error != nil {
                     print(error!)
