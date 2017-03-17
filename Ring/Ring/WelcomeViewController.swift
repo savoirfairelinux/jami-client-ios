7428cb5d2cc8bede80c04b21afe0de560d265843
Silbino Goncalves Matado <sgmatado@gmail.com>
2017-03-17 17:25:52 -0400

    welcome screen: customize the view
    Add WelcomeViewController.swift class into the project.
    Add UILabels and UIImage into the WalkthroughStoryboard and their
    IBOutlets.
    
    Design improvements : Change background and button colors, make
    button width equals, reduce logo width and add content edge insets to
    the RoundedButton. Navigation bar is hidden and the status bar is
    white on the welcome screen.
    
    Tuleap: #1399
    Change-Id: Id5a373f966d0a70522764c0773e6105f32481754

---
+++ Ring/Ring/WelcomeViewController.swift
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Silbino Gonçalves Matado <silbino.gmatado@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var ringImageView: UIImageView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var linkDeviceButton: RoundedButton!
+    @IBOutlet weak var createAccountButton: RoundedButton!
+
+    let createProfileSegueIdentifier = "CreateProfileSegue"
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    @IBAction func createAccountAction(_ sender: Any) {
+        self.performSegue(withIdentifier: createProfileSegueIdentifier, sender: nil)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == createProfileSegueIdentifier {
+            self.navigationController?.setNavigationBarHidden(false, animated: true)
+        }
+    }
+}
