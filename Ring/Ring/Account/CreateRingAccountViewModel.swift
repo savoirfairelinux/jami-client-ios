18e84ef8e0ab55f578078a70177eca02a7835e34
Silbino Goncalves Matado <sgmatado@gmail.com>
2017-03-20 11:30:34 -0400

    CreateRingAccountViewController.swift: change superclass
    Change superclass from UIViewController to UITableViewController
    Add username and password fields
    
    UITableViewController allows to automatically scroll to cells while
    editing UITextField
    
    Also added new cells :
    - TextCell
    - SwitchCell
    - TextFieldCell
    
    Using a simple UIViewController with a UITableView as subview does
    not automatically scroll to the cells
    
    Tuleap: #1400
    Change-Id: I0837e30f006fdfd03aaa5e4914a15aa4f417b7f1

--- Ring/Ring/Account/CreateRingAccountViewModel.swift
+++ Ring/Ring/Account/CreateRingAccountViewModel.swift
@@ -43,6 +43,8 @@ class CreateRingAccountViewModel {
      */
     fileprivate var account: AccountModel?
 
+    var registerUsername = Variable<Bool>(true)
+
     /**
      Default constructor
      */
