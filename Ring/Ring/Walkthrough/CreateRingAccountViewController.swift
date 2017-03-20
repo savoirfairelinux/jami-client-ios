87618655c2c982fc4b6d479a072775dd2f9efd40
Silbino Goncalves Matado <sgmatado@gmail.com>
2017-03-20 11:30:34 -0400

    CreateRingAccountViewController.swift: change superclass
    Change superclass from UIViewController to UITableViewController
    
    UITableViewController allows to automatically scroll to cells while
    editing UITextField
    
    Using a simple UIViewController with a UITableView as subview does
    not automatically scroll to the cells
    
    Tuleap: #1400
    Change-Id: I0837e30f006fdfd03aaa5e4914a15aa4f417b7f1

--- Ring/Ring/Walkthrough/CreateRingAccountViewController.swift
+++ Ring/Ring/Walkthrough/CreateRingAccountViewController.swift
@@ -23,7 +23,7 @@ import UIKit
 import RxCocoa
 import RxSwift
 
-class CreateRingAccountViewController: UIViewController {
+class CreateRingAccountViewController: UITableViewController {
 
     var mAccountViewModel = CreateRingAccountViewModel()
 
@@ -33,8 +33,11 @@ class CreateRingAccountViewController: UIViewController {
         super.viewDidLoad()
 
         self.bindViews()
+        
+        self.setupUI()
+        
     }
-
+    
     /**
      Bind all the necessary of this View to its ViewModel.
      That allows to build the binding part of the MVVM pattern.
@@ -63,6 +66,15 @@ class CreateRingAccountViewController: UIViewController {
                 self?.setCreateAccountAsIdle()
         })
     }
+    
+    /** 
+     Customize the views
+     */
+    
+    fileprivate func setupUI() {
+        self.tableView.estimatedRowHeight = 44.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+    }
 
     fileprivate func setCreateAccountAsLoading() {
         print("Creating account...")
@@ -74,4 +86,20 @@ class CreateRingAccountViewController: UIViewController {
         self.mCreateAccountButton.setTitle("Create a Ring account", for: .normal)
         self.mCreateAccountButton.isUserInteractionEnabled = true
     }
+    
+    //MARK: TableView datasource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchCell", for: indexPath)
+        return cell
+    }
+    
 }
