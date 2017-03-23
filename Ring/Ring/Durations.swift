51063001dddcc03092c8c4e69b0cadf8a1cee9c4
Silbino Goncalves Matado <sgmatado@gmail.com>
2017-03-23 17:19:21 -0400

    add MVVM observation + validation
    add cell scroll
    
    Change-Id: Ib03797e365972aefafbc75c5a8b798e8ba659cfc

---
+++ Ring/Ring/Durations.swift
@@ -0,0 +1,15 @@
+//
+//  Durations.swift
+//  Ring
+//
+//  Created by Silbino Goncalves Matado on 17-03-31.
+//  Copyright © 2017 Savoir-faire Linux. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Time interval between TextField events in seconds
+*/
+
+let textFieldThrottlingDuration = 0.5
