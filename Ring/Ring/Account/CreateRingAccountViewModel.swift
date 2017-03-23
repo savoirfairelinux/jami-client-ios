8644f86ce6b241fcee63c664cf1d3202a9698e21
Silbino Goncalves Matado <sgmatado@gmail.com>
2017-03-23 17:19:21 -0400

    add MVVM observation + validation
    add cell scroll
    
    Change-Id: Ib03797e365972aefafbc75c5a8b798e8ba659cfc

--- Ring/Ring/Account/CreateRingAccountViewModel.swift
+++ Ring/Ring/Account/CreateRingAccountViewModel.swift
@@ -38,6 +38,28 @@ class CreateRingAccountViewModel {
      */
     fileprivate var addAccountDisposable: Disposable?
 
+    var username = Variable<String>("")
+    var password = Variable<String>("")
+    var repeatPassword = Variable<String>("")
+
+    var isPasswordValid :Observable<Bool> {
+            return Observable<Bool>.combineLatest(self.username.asObservable(), self.password.asObservable(), self.repeatPassword.asObservable()) { (username, password, repeatPassword) in
+                return password.characters.count >= 6
+        }
+    }
+
+    var isPasswordsEquals :Observable<Bool> {
+        return Observable<Bool>.combineLatest(self.password.asObservable(), self.repeatPassword.asObservable()) { password, repeatPassword in
+            return password == repeatPassword
+        }
+    }
+
+    var canCreateAccount :Observable<Bool> {
+        return Observable<Bool>.combineLatest(self.isPasswordValid, self.isPasswordsEquals) { isPasswordValid, isPasswordsEquals in
+            return isPasswordValid == isPasswordsEquals
+        }
+    }
+
     /**
      The account under this ViewModel.
      */
