f6ca542a50e66a805ae005bb6a257bf28f04ad4a
Silbino Goncalves Matado <sgmatado@gmail.com>
2017-04-03 13:53:41 -0400

    lookup name: add service and adapter
    Change-Id: I5a66dde2576391b5ec2dc242fb544dc4fe680d9e

---
+++ Ring/Ring/BlockchainService.swift
@@ -0,0 +1,83 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Silbino Gonçalves Matado <silbino.gmatado@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import UIKit
+import RxSwift
+
+/**
+ Represents the status of a username validation request when the user is typing his username
+ */
+enum UsernameValidationStatus {
+    case empty
+    case lookingUp
+    case invalid
+    case alreadyTaken
+    case valid
+}
+
+class BlockchainService: BlockchainAdapterDelegate {
+
+    init(withBlockchainAdapter blockchainAdapter: BlockchainAdapter) {
+        self.blockchainAdapter = blockchainAdapter
+        BlockchainAdapter.delegate = self
+    }
+
+    fileprivate let disposebag = DisposeBag()
+
+    /**
+     Used to make lookup name request to the daemon
+    */
+    fileprivate let blockchainAdapter :BlockchainAdapter
+
+    /**
+     Status of the current username lookup request
+     */
+    var usernameValidationStatus = PublishSubject<UsernameValidationStatus>()
+
+    /**
+    Make a username lookup request to the daemon
+     */
+    func lookupName(with account: String, nameserver: String, name: String) {
+
+        if name.isEmpty {
+            usernameValidationStatus.onNext(.empty)
+        } else {
+            usernameValidationStatus.onNext(.lookingUp)
+            self.blockchainAdapter.lookupName(withAccount: account, nameserver: nameserver, name: name)
+        }
+    }
+
+    //MARK: BlockchainService delegate
+
+    internal func didReceiveLookupNameResponse(_ response: LookupNameResponse) {
+
+        /* If the username is available, the daemon returns an error
+         to indicate that the name is not found in the blockchain
+         */
+
+        if response.state == .error {
+            usernameValidationStatus.onNext(.valid)
+        } else if response.state == .found {
+            usernameValidationStatus.onNext(.alreadyTaken)
+        } else {
+            usernameValidationStatus.onNext(.invalid)
+        }
+    }
+}
--- Ring/Ring/LookupNameResponse.h
+++ Ring/Ring/LookupNameResponse.h
@@ -1,8 +1,7 @@
 /*
- *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
  *
- *  Author: Edric Ladent-Milaret <edric.ladent-milaret@savoirfairelinux.com>
- *  Author: Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
+ *  Author: Silbino Gonçalves Matado <silbino.gmatado@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,11 +18,21 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
-/**
- Expose Objective-C bridging classes to Swift.
- */
-#import "AccountAdapter.h"
-#import "SystemAdapter.h"
-#import "DRingAdapter.h"
+#import <Foundation/Foundation.h>
 #import "BlockchainAdapter.h"
-#import "LookupNameResponse.h"
+
+//Represents the status of the lookup name response from to the daemon
+typedef NS_ENUM(NSInteger, LookupNameState) {
+    found = 0,
+    invalidName,
+    error
+};
+
+@interface LookupNameResponse : NSObject
+
+@property (nonatomic, retain) NSString* accountId;
+@property (nonatomic) LookupNameState state;
+@property (nonatomic, retain) NSString* address;
+@property (nonatomic, retain) NSString* name;
+
+@end
