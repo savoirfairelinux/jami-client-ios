dbfd3edd4ebd20409d49c97b7b78811514698463
Silbino Goncalves Matado <silbino.gmatado@savoirfairelinux.com>
2017-04-24 17:12:01 -0400

    Messages: add adapter and service
    Add adapter and service (send/receive messages + get message status)
    
    Change-Id: I512bfafc7d3e77888a3560a183a085434220337b

--- Ring/Ring/MessagesAdapterDelegate.swift
+++ Ring/Ring/MessagesAdapterDelegate.swift
@@ -1,8 +1,7 @@
 /*
- *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
  *
- *  Author: Edric Ladent-Milaret <edric.ladent-milaret@savoirfairelinux.com>
- *  Author: Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
+ *  Author: Silbino Gonçalves Matado <silbino.gmatado@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,14 +18,11 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
-/**
- Expose Objective-C bridging classes to Swift.
- */
-#import "AccountAdapter.h"
-#import "SystemAdapter.h"
-#import "DRingAdapter.h"
-#import "NameRegistrationAdapter.h"
-#import "LookupNameResponse.h"
-#import "RegistrationResponse.h"
-#import "NameRegistrationResponse.h"
-#import "MessagesAdapter.h"
+@objc protocol MessagesAdapterDelegate {
+
+    func didReceiveMessage(_ message: Dictionary<String, String>, from senderAccount: String,
+                           to receiverAccountId: String)
+
+    func messageStatusChanged(_ status: MessageStatus, for messageId: UInt64, from senderAccountId: String,
+                              to receiverAccount: String)
+}
