3c1115331fbc17aafd5539f1ff618eced7b1db44
Silbino Goncalves Matado <silbino.gmatado@savoirfairelinux.com>
2017-05-18 11:32:20 -0400

    Smartlist: Add conversation filter by username or RingId
    Change-Id: I7985f0644f97063875bfb02159d44dd1aa8b731e

--- Ring/Ring/SmartlistViewModel.swift
+++ Ring/Ring/SmartlistViewModel.swift
@@ -21,24 +21,75 @@
 import UIKit
 import RxSwift
 
-class SmartlistViewModel: NSObject {
+class SmartlistViewModel {
 
     fileprivate let messagesService: MessagesService
 
-    let conversations :Observable<[ConversationViewModel]>
+    fileprivate let disposeBag = DisposeBag()
+
+    fileprivate let conversations = Variable([ConversationModel]())
+
+    fileprivate let searchResults = Variable([ConversationModel]())
+
+    let conversationsViewModels :Observable<[ConversationViewModel]>
+
+    let searchResultsViewModels :Observable<[ConversationViewModel]>
+
+    let searchBarText = Variable<String>("")
+
+    let isSearching :Observable<Bool>
 
     init(withMessagesService messagesService: MessagesService) {
+
         self.messagesService = messagesService
 
-        //Create observable from sorted conversations and flatMap them to view models
-        self.conversations = self.messagesService.conversationsStream.map({ conversations in
+        //Sort Conversations and map them to ConversationViewModels
+        let sortedViewModels: (([ConversationModel]) -> [ConversationViewModel]) = { conversations in
             return conversations.sorted(by: {
                 //TODO: Sort by status
                 return $0.lastMessageDate! > $1.lastMessageDate!
             }).flatMap({ conversationModel in
                 return conversationModel.viewModel
             })
-        }).observeOn(MainScheduler.instance)
+        }
+
+        //Create observable from sorted conversations and flatMap them to view models
+        self.conversationsViewModels = self.conversations.asObservable().map(sortedViewModels).observeOn(MainScheduler.instance)
+
+        //Create observable from sorted conversations and flatMap them to view models
+        self.searchResultsViewModels = self.searchResults.asObservable().map(sortedViewModels).observeOn(MainScheduler.instance)
+
+        self.isSearching = searchBarText.asObservable().map({ text in
+            return text.characters.count > 0
+        })
+
+        //Update new conversations
+        self.messagesService.conversationsStream.subscribe(onNext: { [unowned self] newValue in
+            self.conversations.value = newValue
+        }).addDisposableTo(disposeBag)
+
+        //Observes search bar text
+        searchBarText.asObservable().subscribe(onNext: { [unowned self] text in
+            self.search(withText: text)
+        }).addDisposableTo(disposeBag)
+
     }
 
+    func search(withText text: String) {
+        if text.characters.count == 0 {
+            self.searchResults.value = [ConversationModel]()
+        } else {
+            //Filter conversations by user name or RingId
+            let searchResults = self.conversations.value.filter({ conversation in
+                if let recipientUserName = conversation.recipient.userName {
+                    return recipientUserName.contains(text)
+                } else {
+                    return conversation.recipient.ringId.contains(text)
+                }
+            })
+
+            //Send filtered conversations
+            self.searchResults.value = searchResults
+        }
+    }
 }
