f4f66b1f9228c9ed1650f091abfa9b656f807e14
Hadrien De Sousa <hadriendesousa@Yoda.local>
2017-09-12 17:32:34 -0400

    Me: refactor MeViewController
    This commit refactors the meViewController that now displays the user
    name and the registered name if there is one
    
    It allows the user to pick or take a picture from the device
    
    Change-Id: I0a1bf9c1636a216668dae5de8d78948bea66ead1

---
+++ Ring/Ring/Extensions/UIImage+Helpers.swift
@@ -0,0 +1,48 @@
+//
+//  UIImage+Helpers.swift
+//  Ring
+//
+//  Created by Hadrien De Sousa on 17-07-19.
+//  Copyright © 2017 Savoir-faire Linux. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    var circleMasked: UIImage? {
+        let newSize = self.size
+        
+        let minEdge = min(newSize.height, newSize.width)
+        let size = CGSize(width: minEdge, height: minEdge)
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()
+        
+        self.draw(in: CGRect(origin: CGPoint.zero, size: size), blendMode: .copy, alpha: 1.0)
+        
+        context!.setBlendMode(.copy)
+        context!.setFillColor(UIColor.clear.cgColor)
+        
+        let rectPath = UIBezierPath(rect: CGRect(origin: CGPoint.zero, size: size))
+        let circlePath = UIBezierPath(ovalIn: CGRect(origin: CGPoint.zero, size: size))
+        rectPath.append(circlePath)
+        rectPath.usesEvenOddFillRule = true
+        rectPath.fill()
+        
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return result
+    }
+    // convenience function in UIImage extension to resize a given image
+    func convert(toSize size: CGSize, scale: CGFloat) -> UIImage {
+        let imgRect = CGRect(origin: CGPoint(x:0.0, y:0.0), size: size)
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        self.draw(in: imgRect)
+        let copied = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return copied!
+    }
+}
