330f20d1ff6092e7be6439a52b233c2ff1838230
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2017-09-22 16:29:36 -0400

    ui/ux: modernize fallback avatars
    - Uses a color circle with a white capital letter(the first letter of
      the best id), instead of a character image as a default avatar.
    
    - Maintains the color selection used by the GNOME, UWP, Win32, and
      OSX ring clients(a subset of Google's material.io colors).
    
    Change-Id: I957abd2526e1d1e397f383b93720766e24a92aeb
    Reviewed-by: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>

---
+++ Ring/Ring/Constants/AvatarsColors.swift
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import Foundation
+
+// Colors from material.io
+let avatarColors = [
+    UIColor(hexString: "#f44336"), // Red
+    UIColor(hexString: "#e91e63"), // Pink
+    UIColor(hexString: "#9c27b0"), // Purple
+    UIColor(hexString: "#673ab7"), // Deep Purple
+    UIColor(hexString: "#3f51b5"), // Indigo
+    UIColor(hexString: "#2196f3"), // Blue
+    UIColor(hexString: "#00bcd4"), // Cyan
+    UIColor(hexString: "#009688"), // Teal
+    UIColor(hexString: "#4caf50"), // Green
+    UIColor(hexString: "#8bc34a"), // Light Green
+    UIColor(hexString: "#9e9e9e"), // Grey
+    UIColor(hexString: "#cddc39"), // Lime
+    UIColor(hexString: "#ffc107"), // Amber
+    UIColor(hexString: "#ff5722"), // Deep Orange
+    UIColor(hexString: "#795548"), // Brown
+    UIColor(hexString: "#607d8b")  // Blue Grey
+];
+
+let defaultAvatarColor = UIColor(hexString: "808080")
