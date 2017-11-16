01a7a032bbc3b4d91db7fbf72708144ed81461b9
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2017-11-16 15:59:19 -0500

    accountsService: introduce new AccountsService
    This new accountsService is designed to avoid any model retention /
    duplication from the daemon.
    The daemon is used as a repository.
    
    It now takes the delegation from the accountAdapter, making the old
    accountAdapter not responding to the daemon events.
    
    This new service will be used more and more to finally replace the old
    one as soon as possible. For now, the two coexist.
    
    Change-Id: I5a925312254c1b2df1c51966a1282da7850a075d
    Tuleap: #1752

--- Ring/Ring/Services/ServiceEvent.swift
+++ Ring/Ring/Services/ServiceEvent.swift
@@ -38,6 +38,7 @@ enum ServiceEventType {
  Keys that can be set as keys of the private input dictionary
  */
 enum ServiceEventInput {
+    case accountId
     case id
     case state
     case registrationState
