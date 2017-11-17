9a743e11040cf5b857687aba66bb9446652efb33
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2017-11-17 14:59:42 -0500

    project: introduce initial loading interface
    This commit adds an initial loading storyboard and VC to the project. It
    will be useful to inform the user that the app is initializing.
    
    Change-Id: Ic3bf26b09b1a997caab913d22bce1623ed543095
    Tuleap: #1752

---
+++ Ring/Ring/Features/InitialLoading/InitialLoadingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InitialLoadingViewController.swift
+//  Ring
+//
+//  Created by Romain Bertozzi on 17-11-13.
+//  Copyright © 2017 Savoir-faire Linux. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+final class InitialLoadingViewController: UIViewController, StoryboardBased {
+
+}
