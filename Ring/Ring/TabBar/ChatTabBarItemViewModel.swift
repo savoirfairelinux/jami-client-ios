1451e0a389d0b155322d5fdbbabfc31d57c5e3fc
Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
2017-11-20 16:48:36 -0500

    replace old accounts service in tabbar badges
    Change-Id: Id3d7a16cdbdc57f386d8241a1cc790bb0ed18df6

--- Ring/Ring/TabBar/ChatTabBarItemViewModel.swift
+++ Ring/Ring/TabBar/ChatTabBarItemViewModel.swift
@@ -2,6 +2,7 @@
  *  Copyright (C) 2017 Savoir-faire Linux Inc.
  *
  *  Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
+ *  Author: Romain Bertozzi <romain.bertozzi@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,38 +19,57 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
-import Foundation
 import RxSwift
+import SwiftyBeaver
 
-class ChatTabBarItemViewModel: ViewModel, TabBarItemViewModel {
+final class ChatTabBarItemViewModel: ViewModel, TabBarItemViewModel {
 
-    var itemBadgeValue: Observable<String?>
+    private let log = SwiftyBeaver.self
+
+    private let itemBadgeValue: Variable<String?> = Variable(nil)
+    lazy var itemBadgeValueObservable: Observable<String?> = {
+        return self.itemBadgeValue.asObservable()
+    }()
+
+    private let conversationsService: ConversationsService
+    private let accountsService: NewAccountsService
+    private let contactsService: ContactsService
+
+    private let disposeBag = DisposeBag()
 
     required init(with injectionBag: InjectionBag) {
-        let accountService = injectionBag.accountService
-        let conversationService = injectionBag.conversationsService
-        let contactsService = injectionBag.contactsService
-        self.itemBadgeValue = {
-            return conversationService.conversationsForCurrentAccount.map({ conversations in
-                return conversations.map({ conversation in
-                    let unreadMsg = conversation.messages.filter({ message in
-                        if let account = accountService.currentAccount {
-                            let accountHelper = AccountModelHelper(withAccount: account)
-                            //filtre out read messages, outgoing messages and messages that are displayed in contactrequest conversation
-                            return message.status != .read && message.author != accountHelper.ringId
-                                && (contactsService.contactRequest(withRingId: message.author) == nil)
-                        }
-                        return false
-                    })
-                    return unreadMsg.count
-                }).reduce(0, +)
-            })
-            }()
-            .map { number in
-                     if number == 0 {
-                         return nil
-                     }
-                return "\(number)"
-            }
+        self.conversationsService = injectionBag.conversationsService
+        self.accountsService = injectionBag.newAccountsService
+        self.contactsService = injectionBag.contactsService
+
+        self.registerToConversations()
     }
+
+    private func registerToConversations() {
+        let currentAccountObservable = self.accountsService.currentAccount().asObservable()
+        let conversationsObservable = self.conversationsService.conversations.asObservable()
+
+        let unreadMessagesObservable = Observable.combineLatest(currentAccountObservable, conversationsObservable) { [weak self] (account, conversations) -> Int in
+            return conversations.map({ (conversation) -> Int in
+                guard let accountRingId = AccountModelHelper(withAccount: account).ringId else {
+                    return 0
+                }
+                guard let contactsService = self?.contactsService else {
+                    return 0
+                }
+                return ConversationModelHelper.getNumberOfUnreadMessages(for: conversation,
+                                                                         currentAccountRingId: accountRingId,
+                                                                         contactsService: contactsService)
+            }).reduce(0, +)
+        }
+
+        unreadMessagesObservable.subscribe(onNext: { [weak self] (value) in
+            let unreadMessages = value == 0 ? nil : String(value)
+            self?.itemBadgeValue.value = unreadMessages
+        }, onError: { [weak self] (error) in
+            self?.itemBadgeValue.value = nil
+            self?.log.debug("Empty chat badge: \(error.localizedDescription)")
+        }).disposed(by: self.disposeBag)
+    }
+
 }
