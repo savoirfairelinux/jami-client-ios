2c2f61368e06d76f80f4cf48a000293a358a1b69
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2017-11-22 14:42:41 -0500

    database: create database and tables
    this patch:
    - adds sqlite.swift to dependencies
    - creates database
    - creates tables
    - adds basics queries
    
    Change-Id: I178097aebaa62d73886132ee7a86bb4a9734d25b

---
+++ Ring/Ring/Models/ConversationModel1.swift
@@ -0,0 +1,22 @@
+//
+//  ConversationModel1.swift
+//  Ring
+//
+//  Created by Kateryna Kostiuk on 2017-11-23.
+//  Copyright © 2017 Savoir-faire Linux. All rights reserved.
+//
+
+import Foundation
+
+class ConversationModel1 {
+    var messages = [MessageModel]()
+    var recipientRingId: String = ""
+    var accountId: String = ""
+    var participantProfile: Profile?
+
+    convenience init(withRecipientRingId recipientRingId: String, accountId: String) {
+        self.init()
+        self.recipientRingId = recipientRingId
+        self.accountId = accountId
+    }
+}
