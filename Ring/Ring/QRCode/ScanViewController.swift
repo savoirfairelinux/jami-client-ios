834a17fc3dec62bfb253b08112db4aa0c03e3c67
qmuret <quentin.muret@savoirfairelinux.com>
2018-09-17 15:25:34 -0400

    qr-code-feature-ok
    Change-Id: Ica1238ab8ed488d2222cca0cd8099b2484dc085d

--- Ring/Ring/QRCode/ScanViewController.swift
+++ Ring/Ring/QRCode/ScanViewController.swift
@@ -14,24 +14,25 @@ import AudioToolbox
 class ScanViewController: UIViewController, StoryboardBased, AVCaptureMetadataOutputObjectsDelegate {
 
     @IBOutlet weak var infoLbl: UILabel!
+    @IBOutlet weak var header: UIView!
     
     let systemSoundId : SystemSoundID = 1016
 
     //captureSession manages capture activity and coordinates between input device and captures outputs
     var captureSession:AVCaptureSession?
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
-
-    //Empty Rectangle with green border to outline detected QR or BarCode
+    //Empty Rectangle with border to outline detected QR or BarCode
+    
     lazy var codeFrame:UIView = {
         let cFrame = UIView()
-        cFrame.layer.borderColor = UIColor.red.cgColor
-        cFrame.layer.borderWidth = 1.5
+        cFrame.layer.borderColor = UIColor.cyan.cgColor
+        cFrame.layer.borderWidth = 2
+        cFrame.layer.cornerRadius = 4
         cFrame.frame = CGRect.zero
         cFrame.translatesAutoresizingMaskIntoConstraints = false
         return cFrame
     }()
 
-    
     override func viewDidDisappear(_ animated: Bool) {
         captureSession?.stopRunning()
     }
@@ -39,30 +40,32 @@ class ScanViewController: UIViewController, StoryboardBased, AVCaptureMetadataOu
     override func viewDidAppear(_ animated: Bool) {
         captureSession?.startRunning()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         //AVCaptureDevice allows us to reference a physical capture device (video in our case)
         let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
 
         if let captureDevice = captureDevice {
-            
+
             do {
 
                 captureSession = AVCaptureSession()
-                
+
                 // CaptureSession needs an input to capture Data from
                 let input = try AVCaptureDeviceInput(device: captureDevice)
                 captureSession?.addInput(input)
-                
+
                 // CaptureSession needs and output to transfer Data to
                 let captureMetadataOutput = AVCaptureMetadataOutput()
                 captureSession?.addOutput(captureMetadataOutput)
 
                 //We tell our Output the expected Meta-data type
                 captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-                captureMetadataOutput.metadataObjectTypes = [.code128, .qr,.ean13, .ean8, .code39, .upce, .aztec, .pdf417] //AVMetadataObject.ObjectType
+                captureMetadataOutput.metadataObjectTypes = [.code128, .qr,.ean13, .ean8, .code39, .upce, .aztec, .pdf417]
+                //AVMetadataObject.ObjectType
 
                 captureSession?.startRunning()
 
@@ -72,8 +75,8 @@ class ScanViewController: UIViewController, StoryboardBased, AVCaptureMetadataOu
                 videoPreviewLayer?.frame = view.layer.bounds
                 view.layer.addSublayer(videoPreviewLayer!)
                 view.bringSubview(toFront: infoLbl)
+                view.bringSubview(toFront: header)
             }
-
 
             catch {
 
@@ -81,7 +84,7 @@ class ScanViewController: UIViewController, StoryboardBased, AVCaptureMetadataOu
 
             }
         }
-        
+
     }
 
     // the metadataOutput function informs our delegate (the ScanViewController) that the captureOutput emitted a new metaData Object
@@ -97,7 +100,7 @@ class ScanViewController: UIViewController, StoryboardBased, AVCaptureMetadataOu
         }
 
         view.addSubview(codeFrame)
-        
+
         //transformedMetaDataObject returns layer coordinates/height/width from visual properties
         guard let metaDataCoordinates = videoPreviewLayer?.transformedMetadataObject(for: metaDataObject) else {
             return
@@ -106,17 +109,12 @@ class ScanViewController: UIViewController, StoryboardBased, AVCaptureMetadataOu
         //Those coordinates are assigned to our codeFrame
         codeFrame.frame = metaDataCoordinates.bounds
         AudioServicesPlayAlertSound(systemSoundId)
-        infoLbl.text = stringCodeValue
-        //        if let url = URL(string: stringCodeValue) {
-        //            performSegue(withIdentifier: "segToDetailsVC", sender: self)
-        //            captureSession?.stopRunning()
-        //        }
-        print(infoLbl.text)
+        let ringId = stringCodeValue.components(separatedBy: "http://").last!
+        infoLbl.text = ringId
+        print(ringId)
     }
 
-    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    //        if let nextVC = segue.destination as? DetailsViewController {
-    //            nextVC.scannedCode = infoLbl.text
-    //        }
-    //    }
+    @IBAction func closeScan(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
