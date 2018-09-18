28c7be4bbf2f267a803ea26a95feace29b437206
qmuret <quentin.muret@savoirfairelinux.com>
2018-09-18 14:32:45 -0400

    Added Emoji+Helpers extension
    Change-Id: I6ec491e0120180de9d2dc7bc51e3c3c74dc70103

---
+++ Ring/Ring/Extensions/Emoji+Helpers.swift
@@ -0,0 +1,113 @@
+//
+//  Emoji+Helpers.swift
+//  Ring
+//
+//  Created by Quentin on 2018-09-26.
+//  Copyright © 2018 Savoir-faire Linux. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UnicodeScalar {
+
+    var isEmoji: Bool {
+
+        switch value {
+        case 0x1F000...0x1FFFF, // Emoticons
+        0x2600...0x26FF,   // Misc symbols
+        0x2700...0x27BF,   // Dingbats
+        0xFE00...0xFE0F,   // Variation Selectors
+        127000...127600, // Various asian characters
+        65024...65039, // Variation selector
+        9100...9300, // Misc items
+        8400...8447: // Combining Diacritical Marks for Symbols
+            return true
+
+        default: return false
+        }
+    }
+
+    var isZeroWidthJoiner: Bool {
+
+        return value == 8205
+    }
+}
+
+extension String {
+
+    var glyphCount: Int {
+
+        let richText = NSAttributedString(string: self)
+        let line = CTLineCreateWithAttributedString(richText)
+        return CTLineGetGlyphCount(line)
+    }
+
+    var isSingleEmoji: Bool {
+        return glyphCount == 1 && containsEmoji
+    }
+
+    var containsEmoji: Bool {
+
+        return unicodeScalars.contains { $0.isEmoji }
+    }
+
+    var containsOnlyEmoji: Bool {
+
+        return !isEmpty
+            && !unicodeScalars.contains(where: {
+                !$0.isEmoji
+                    && !$0.isZeroWidthJoiner
+            })
+    }
+
+    // The next tricks are mostly to demonstrate how tricky it can be to determine emoji's
+    // If anyone has suggestions how to improve this, please let me know
+    var emojiString: String {
+
+        return emojiScalars.map { String($0) }.reduce("", +)
+    }
+
+    var emojis: [String] {
+
+        var scalars: [[UnicodeScalar]] = []
+        var currentScalarSet: [UnicodeScalar] = []
+        var previousScalar: UnicodeScalar?
+
+        for scalar in emojiScalars {
+
+            if let prev = previousScalar, !prev.isZeroWidthJoiner && !scalar.isZeroWidthJoiner {
+
+                scalars.append(currentScalarSet)
+                currentScalarSet = []
+            }
+            currentScalarSet.append(scalar)
+
+            previousScalar = scalar
+        }
+
+        scalars.append(currentScalarSet)
+
+        return scalars.map { $0.map{ String($0) } .reduce("", +) }
+    }
+
+    fileprivate var emojiScalars: [UnicodeScalar] {
+
+        var chars: [UnicodeScalar] = []
+        var previous: UnicodeScalar?
+        for cur in unicodeScalars {
+
+            if let previous = previous, previous.isZeroWidthJoiner && cur.isEmoji {
+                chars.append(previous)
+                chars.append(cur)
+
+            } else if cur.isEmoji {
+                chars.append(cur)
+            }
+
+            previous = cur
+        }
+
+        return chars
+    }
+}
