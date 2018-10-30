b36500ab12b2c1fe767b80c5f620e67ff5b82274
qmuret <quentin.muret@savoirfairelinux.com>
2018-10-30 17:01:38 -0400

    refactor call view
    Change-Id: I37b634782e3ea5f5eb2afd1f8f1809924435e445

--- Ring/Ring/Calls/CallViewController.swift
+++ Ring/Ring/Calls/CallViewController.swift
@@ -32,6 +32,7 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var durationLabel: UILabel!
     @IBOutlet private weak var infoBottomLabel: UILabel!
+    @IBOutlet weak var avatarView: UIView!
 
     @IBOutlet private weak var mainView: UIView!
 
@@ -39,14 +40,11 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
     @IBOutlet private weak var callView: UIView!
     @IBOutlet private weak var incomingVideo: UIImageView!
     @IBOutlet private weak var capturedVideo: UIImageView!
+    @IBOutlet weak var backgroundCapturedVideo: UIImageView!
     @IBOutlet private weak var infoContainer: UIView!
     @IBOutlet private weak var callProfileImage: UIImageView!
-    @IBOutlet private weak var audioOnlyImage: UIImageView!
     @IBOutlet private weak var callNameLabel: UILabel!
     @IBOutlet private weak var callInfoTimerLabel: UILabel!
-    @IBOutlet private weak var infoLabelTopConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var callButtonsLeftConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var callButtonsRightConstraint: NSLayoutConstraint!
     @IBOutlet private weak var infoLabelHeightConstraint: NSLayoutConstraint!
     @IBOutlet private weak var callPulse: UIView!
 
@@ -71,7 +69,11 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
         self.setupBindings()
         if self.viewModel.isAudioOnly {
             self.showAllInfo()
+            self.setWhiteAvatarView()
+        } else {
+            UIApplication.shared.statusBarStyle = .lightContent
         }
+
         UIDevice.current.isProximityMonitoringEnabled = self.viewModel.isAudioOnly
 
         initCallAnimation()
@@ -79,7 +81,15 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        UIApplication.shared.statusBarStyle = .lightContent
+    }
+
+    func setWhiteAvatarView() {
+                UIApplication.shared.statusBarStyle = .default
+                self.callPulse.backgroundColor = UIColor.ringCallPulse
+                self.avatarView.backgroundColor = UIColor.white
+                self.nameLabel.textColor = UIColor.ringCallInfos
+                self.durationLabel.textColor = UIColor.ringCallInfos
+                self.infoBottomLabel.textColor = UIColor.ringCallInfos
     }
 
     func initCallAnimation() {
@@ -225,6 +235,7 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
                 if let image = frame {
                     DispatchQueue.main.async {
                         self?.capturedVideo.image = image
+                        self?.backgroundCapturedVideo.image = image
                     }
                 }
             }).disposed(by: self.disposeBag)
@@ -244,8 +255,14 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
                     self.showCancelButton()
                 } else if !self.viewModel.isAudioOnly {
                     self.hideCancelButton()
-                } else {
-                    self.buttonsContainer.bottomSpaceConstraint.constant = 30
+                }
+            }).disposed(by: self.disposeBag)
+
+        self.viewModel.showCapturedFrame
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { dontShow in
+                if dontShow {
+                    self.backgroundCapturedVideo.isHidden = true
                 }
             }).disposed(by: self.disposeBag)
 
@@ -254,12 +271,14 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
             .bind(to: self.capturedVideo.rx.isHidden)
             .disposed(by: self.disposeBag)
 
-        self.audioOnlyImage.isHidden = !self.viewModel.isAudioOnly
+        if !self.viewModel.isAudioOnly {
+            self.viewModel.callPaused
+                .observeOn(MainScheduler.instance)
+                .map({value in return !value })
+                .bind(to: self.avatarView.rx.isHidden)
+                .disposed(by: self.disposeBag)
+        }
 
-        self.viewModel.callPaused
-            .observeOn(MainScheduler.instance)
-            .bind(to: self.callView.rx.isHidden)
-            .disposed(by: self.disposeBag)
         self.viewModel.callPaused
             .observeOn(MainScheduler.instance)
             .subscribe(onNext: { [unowned self] show in
@@ -281,13 +300,11 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
 
     func showCancelButton() {
         self.buttonsContainer.isHidden = false
-        self.buttonsContainer.bottomSpaceConstraint.constant = 90
         self.view.layoutIfNeeded()
     }
 
     func hideCancelButton() {
         self.buttonsContainer.isHidden = true
-        self.buttonsContainer.bottomSpaceConstraint.constant = 30
         self.view.layoutIfNeeded()
     }
 
@@ -315,19 +332,15 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
             self.hideContactInfo()
             return
         }
-        self.infoLabelTopConstraint.constant = -200.00
-        self.callButtonsRightConstraint.constant = self.view.bounds.width
-        self.callButtonsLeftConstraint.constant = -self.view.bounds.width
         self.buttonsContainer.isHidden = false
         self.infoContainer.isHidden = false
         self.view.layoutIfNeeded()
 
-        UIView.animate(withDuration: 0.2, delay: 0.0,
+        UIView.animate(withDuration: 0.4, delay: 0.0,
                        options: .curveEaseOut,
                        animations: { [unowned self] in
-                        self.infoLabelTopConstraint.constant = 0.00
-                        self.callButtonsRightConstraint.constant = 0.00
-                        self.callButtonsLeftConstraint.constant = 0.00
+                        self.infoContainer.alpha = 1
+                        self.buttonsContainer.alpha = 1
                         self.view.layoutIfNeeded()
             }, completion: nil)
 
@@ -336,12 +349,11 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
     }
 
     func hideContactInfo() {
-        UIView.animate(withDuration: 0.2, delay: 0.00,
+        UIView.animate(withDuration: 0.4, delay: 0.00,
                        options: .curveEaseOut,
                        animations: { [unowned self] in
-                        self.infoLabelTopConstraint.constant = -200.00
-                        self.callButtonsRightConstraint.constant = self.view.bounds.width
-                        self.callButtonsLeftConstraint.constant = -self.view.bounds.width
+                        self.infoContainer.alpha = 0
+                        self.buttonsContainer.alpha = 0
                         self.view.layoutIfNeeded()
             }, completion: { [weak self] _ in
                 self?.infoContainer.isHidden = true
@@ -352,7 +364,6 @@ class CallViewController: UIViewController, StoryboardBased, ViewModelBased {
     func showAllInfo() {
         self.buttonsContainer.isHidden = false
         self.infoContainer.isHidden = false
-        self.infoLabelTopConstraint.constant = 0.00
     }
 
     @objc func canRotate() {
