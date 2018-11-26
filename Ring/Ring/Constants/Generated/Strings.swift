8c52b6612d0b85137a59ae790dcf1c9d374e7650
qmuret <quentin.muret@savoirfairelinux.com>
2018-11-26 16:05:13 -0500

    UI / UX: refactor account/profile creation views
    Change-Id: I4fecb7bc5bfda5585ce7a4c83ad3a64a2e6b246c

--- Ring/Ring/Constants/Generated/Strings.swift
+++ Ring/Ring/Constants/Generated/Strings.swift
@@ -126,9 +126,9 @@ internal enum L10n {
   internal enum Createaccount {
     /// Choose strong password you will remember to protect your Ring account.
     internal static let chooseStrongPassword = L10n.tr("Localizable", "createAccount.chooseStrongPassword")
-    /// Create your Ring account
+    /// Create your jami account
     internal static let createAccountFormTitle = L10n.tr("Localizable", "createAccount.createAccountFormTitle")
-    /// username
+    /// Username
     internal static let enterNewUsernamePlaceholder = L10n.tr("Localizable", "createAccount.enterNewUsernamePlaceholder")
     /// invalid username
     internal static let invalidUsername = L10n.tr("Localizable", "createAccount.invalidUsername")
@@ -136,13 +136,13 @@ internal enum L10n {
     internal static let loading = L10n.tr("Localizable", "createAccount.loading")
     /// looking for username availability
     internal static let lookingForUsernameAvailability = L10n.tr("Localizable", "createAccount.lookingForUsernameAvailability")
-    /// password
+    /// Password
     internal static let newPasswordPlaceholder = L10n.tr("Localizable", "createAccount.newPasswordPlaceholder")
     /// 6 characters minimum
     internal static let passwordCharactersNumberError = L10n.tr("Localizable", "createAccount.passwordCharactersNumberError")
     /// passwords do not match
     internal static let passwordNotMatchingError = L10n.tr("Localizable", "createAccount.passwordNotMatchingError")
-    /// confirm password
+    /// Confirm password
     internal static let repeatPasswordPlaceholder = L10n.tr("Localizable", "createAccount.repeatPasswordPlaceholder")
     /// username already taken
     internal static let usernameAlreadyTaken = L10n.tr("Localizable", "createAccount.usernameAlreadyTaken")
@@ -246,13 +246,13 @@ internal enum L10n {
   }
 
   internal enum Welcome {
-    /// Create a Ring account
+    /// Create a jami account
     internal static let createAccount = L10n.tr("Localizable", "welcome.createAccount")
     /// Link this device to an account
     internal static let linkDevice = L10n.tr("Localizable", "welcome.linkDevice")
     /// Ring is a free and universal communication platform which preserves the users' privacy and freedoms
     internal static let text = L10n.tr("Localizable", "welcome.text")
-    /// Welcome to Ring
+    /// Welcome to jami !
     internal static let title = L10n.tr("Localizable", "welcome.title")
   }
 }
--- Ring/Ring/Extensions/UIView+Rx.swift
+++ Ring/Ring/Extensions/UIView+Rx.swift
@@ -40,3 +40,54 @@ extension Reactive where Base : UIView {
         }.asObserver()
     }
 }
+
+extension UIView {
+
+    func applyGradient(with colours: [UIColor], locations: [NSNumber]? = nil) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.locations = locations
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+
+    func applyGradient(with colours: [UIColor], gradient orientation: GradientOrientation) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.startPoint = orientation.startPoint
+        gradient.endPoint = orientation.endPoint
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
+
+typealias GradientPoints = (startPoint: CGPoint, endPoint: CGPoint)
+
+enum GradientOrientation {
+    case topRightBottomLeft
+    case topLeftBottomRight
+    case horizontal
+    case vertical
+
+    var startPoint : CGPoint {
+        return points.startPoint
+    }
+
+    var endPoint : CGPoint {
+        return points.endPoint
+    }
+
+    var points : GradientPoints {
+        switch self {
+        case .topRightBottomLeft:
+            return (CGPoint(x: 0.0,y: 1.0), CGPoint(x: 1.0,y: 0.0))
+        case .topLeftBottomRight:
+            return (CGPoint(x: 0.0,y: 0.0), CGPoint(x: 1,y: 1))
+        case .horizontal:
+            return (CGPoint(x: 0.0,y: 0.5), CGPoint(x: 1.0,y: 0.5))
+        case .vertical:
+            return (CGPoint(x: 0.0,y: 0.0), CGPoint(x: 0.0,y: 1.0))
+        }
+    }
+}
+
