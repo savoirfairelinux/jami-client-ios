dc7aab4ce6f6f6698306c776b3a52dc6f6dfc26e
Quentin Muret <quentin.muret@savoirfairelinux.com>
2018-12-14 17:30:14 -0500

    change my personal adresse to my professionnellle
    Change-Id: I76f36b55be4da30a86fc75524f4785afdf52d5a8

--- Ring/Ring/Extensions/UIImage+Helpers.swift
+++ Ring/Ring/Extensions/UIImage+Helpers.swift
@@ -3,7 +3,7 @@
  *
  *  Author: Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
  *  Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
- *  Author: Quentin Muret <quentin.muret@gmail.com>
+ *  Author: Quentin Muret <quentin.muret@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
