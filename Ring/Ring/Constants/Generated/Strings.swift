61c8a970afcf8fbc315353bdd11731a138be90ef
Raphaël Brulé <raphael.brule@savoirfairelinux.com>
2020-06-22 17:12:56 -0400

    Swiftgen: bump to Swift5
    Bumps templates swift3 and structured-swift3 to
    swift5 and structured-swift5 to work with
    swiftgen version 6.2.0.
    
    Change-Id: Ic7e35377e7fdc555dcc8a63cc253f41539cc0dad

--- Ring/Ring/Constants/Generated/Strings.swift
+++ Ring/Ring/Constants/Generated/Strings.swift
@@ -3,8 +3,7 @@
 
 import Foundation
 
-// swiftlint:disable superfluous_disable_command
-// swiftlint:disable file_length
+// swiftlint:disable superfluous_disable_command file_length implicit_return
 
 // MARK: - Strings
 
@@ -563,11 +562,16 @@ internal enum L10n {
 // MARK: - Implementation Details
 
 extension L10n {
-  fileprivate static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    // swiftlint:disable:next nslocalizedstring_key
-    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
 
-private final class BundleToken {}
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    Bundle(for: BundleToken.self)
+  }()
+}
+// swiftlint:enable convenience_type
