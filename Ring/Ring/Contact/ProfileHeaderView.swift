61a45b0c88280ddf9697808b627feed4562b0ed1
Raphaël Brulé <raphael.brule@savoirfairelinux.com>
2020-08-17 15:55:25 -0400

    jamiID: see the jami ID on contact page
    Adds a label for the jamiID on contact's page.
    
    Change-Id: I5a67b92078d3920a7de36d8dd4a0b64b4762c0de
    Gitlab: #78

--- Ring/Ring/Contact/ProfileHeaderView.swift
+++ Ring/Ring/Contact/ProfileHeaderView.swift
@@ -2,6 +2,7 @@
  *  Copyright (C) 2018-2019 Savoir-faire Linux Inc.
  *
  *  Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
+ *  Author: Raphaël Brulé <raphael.brule@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,12 +24,13 @@ class ProfileHeaderView: GSKStretchyHeaderView {
     @IBOutlet var avatarView: UIView!
     @IBOutlet var displayName: UILabel!
     @IBOutlet var userName: UILabel!
+    @IBOutlet weak var jamiID: CopyableLabel!
     @IBOutlet weak var background: UIView!
 
     override func willMove(toWindow newWindow: UIWindow?) {
         super.willMove(toWindow: newWindow)
         self.backgroundColor = UIColor.jamiBackgroundColor
-        background.backgroundColor = UIColor.jamiBackgroundColor
+        self.background.backgroundColor = UIColor.jamiBackgroundColor
     }
 
     override func didChangeStretchFactor(_ stretchFactor: CGFloat) {
@@ -37,11 +39,13 @@ class ProfileHeaderView: GSKStretchyHeaderView {
         self.avatarView.alpha = alpha
         self.displayName.alpha = alpha
         self.userName.alpha = alpha
+        self.jamiID.alpha = alpha
 
         var scale = CGFloatTranslateRange(stretchFactor, 0.1, 0.9, 0.6, 1)
         scale = max(0.4, min(1, scale))
         self.avatarView.transform = CGAffineTransform(scaleX: scale, y: scale)
         self.displayName.transform = CGAffineTransform(scaleX: scale, y: scale)
         self.userName.transform = CGAffineTransform(scaleX: scale, y: scale)
+        self.jamiID.transform = CGAffineTransform(scaleX: scale, y: scale)
     }
 }
