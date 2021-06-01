1054c37adf2e11e3bd8a7b87abb43bd62f369a2e
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-06-01 09:22:13 -0400

    ios s
    Change-Id: Id9b12c6a51a4d427851ea9494bfd34d72da8a1ca

--- Ring/Ring/Services/AccountsService.swift
+++ Ring/Ring/Services/AccountsService.swift
@@ -963,6 +963,20 @@ class AccountsService: AccountAdapterDelegate {
         self.switchAccountPropertyTo(state: enable, accountId: accountId, property: ConfigKeyModel(withKey: ConfigKey.dhtPeerDiscovery))
     }
 
+    func enableTurn(enable: Bool, accountId: String) {
+        self.switchAccountPropertyTo(state: enable, accountId: accountId, property: ConfigKeyModel(withKey: ConfigKey.turnEnable))
+    }
+
+    func setTurnSettings(accountId: String, server: String, username: String, password: String, realm: String) {
+        let details = self.accountService.getAccountDetails(fromAccountId: accountId)
+        details.set(withConfigKeyModel: ConfigKeyModel(withKey: ConfigKey.turnServer), withValue: server)
+        details.set(withConfigKeyModel: ConfigKeyModel(withKey: ConfigKey.turnUsername), withValue: username)
+        details.set(withConfigKeyModel: ConfigKeyModel(withKey: ConfigKey.turnPassword), withValue: password)
+        details.set(withConfigKeyModel: ConfigKeyModel(withKey: ConfigKey.turnRealm), withValue: realm)
+        account.details = details
+        self.accountService.setAccountDetails(forAccountId: accountId, withDetails: details)
+    }
+
     func enableKeepAlive(enable: Bool, accountId: String) {
         self.switchAccountPropertyTo(state: enable, accountId: accountId, property: ConfigKeyModel(withKey: ConfigKey.keepAliveEnabled))
     }
