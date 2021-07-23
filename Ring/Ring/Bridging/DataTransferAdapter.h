f91f673009db7c96501f105b5085c7ac4911ab60
kkostiuk <kateryna.kostiuk@savoirfairelinux.com>
2021-07-23 15:34:29 -0400

    swarm support
    Change-Id: I043ed735391df8e02bb97bbc40ec040937ffaaf2

--- Ring/Ring/Bridging/DataTransferAdapter.h
+++ Ring/Ring/Bridging/DataTransferAdapter.h
@@ -51,21 +51,23 @@ typedef NS_ENUM(UInt32, NSDataTransferFlags)  {
     NSString* accountId;
     NSDataTransferEventCode lastEvent;
     UInt32 flags;
-    SInt64 totalSize;
-    SInt64 bytesProgress;
+    int64_t totalSize;
+    int64_t bytesProgress;
     NSString* peer;
     NSString* displayName;
     NSString* path;
     NSString* mimetype;
+    NSString* conversationId;
 }
 @property (strong, nonatomic) NSString* accountId;
 @property (nonatomic) NSDataTransferEventCode lastEvent;
 @property (nonatomic) UInt32 flags;
-@property (nonatomic) SInt64 totalSize;
-@property (nonatomic) SInt64 bytesProgress;
+@property (nonatomic) int64_t totalSize;
+@property (nonatomic) int64_t bytesProgress;
 @property (strong, nonatomic) NSString* peer;
 @property (strong, nonatomic) NSString* displayName;
 @property (strong, nonatomic) NSString* path;
+@property (strong, nonatomic) NSString* conversationId;
 @property (strong, nonatomic) NSString* mimetype;
 @end
 
@@ -75,11 +77,34 @@ typedef NS_ENUM(UInt32, NSDataTransferFlags)  {
 
 @property (class, nonatomic, weak) id <DataTransferAdapterDelegate> delegate;
 
-- (NSArray*)dataTransferList;
-- (NSDataTransferError) sendFileWithInfo:(NSDataTransferInfo*)info withTransferId:(UInt64*)transferId;
-- (NSDataTransferError) acceptFileTransferWithId:(UInt64)transferId withFilePath:(NSString*)filePath withOffset:(SInt64)offset;
-- (NSDataTransferError) cancelDataTransferWithId:(UInt64)transferId;
-- (NSDataTransferError) dataTransferInfoWithId:(UInt64)transferId withInfo:(NSDataTransferInfo*)info;
-- (NSDataTransferError) dataTransferBytesProgressWithId:(UInt64)transferId withTotal:(SInt64*)total withProgress:(SInt64*)progress;
+///swarm conversations
+- (void)sendSwarmFileWithName:(NSString*)displayName
+                    accountId:(NSString*)accountId
+               conversationId:(NSString*)conversationId
+                 withFilePath:(NSString*)filePath
+                       parent:(NSString*)parent;
 
+- (NSDataTransferError) swarmTransferProgressWithId:(NSString*)fileId
+                                          accountId:(NSString*)accountId
+                                           withInfo:(NSDataTransferInfo*)info;
+
+- (bool)downloadSwarmTransferWithFileId:(NSString*)fileId
+                              accountId:(NSString*)accountId
+                         conversationId:(NSString*)conversationId
+                          interactionId:(NSString*)interactionId
+                           withFilePath:(NSString*)filePath;
+
+///non swarm conversations
+- (NSDataTransferError)sendNonSwarmFileWithInfo:(NSDataTransferInfo*)info withTransferId:(UInt64*)transferId;
+- (NSDataTransferError)acceptNonSwarmTransferWithId:(NSString*)fileId
+                                          accountId:(NSString*)accountId
+                                       withFilePath:(NSString*)filePath;
+- (NSDataTransferError)nonSwarmTransferInfoWithId:(NSString*)fileId
+                                        accountId:(NSString*)accountId
+                                         withInfo:(NSDataTransferInfo*)info;
+
+///swarm and non swarm conversations
+- (NSDataTransferError)cancelDataTransferWithId:(NSString*)fileId
+                                      accountId:(NSString*)accountId
+                                 conversationId:(NSString*)conversationId;
 @end
--- Ring/Ring/Models/MessageModel.swift
+++ Ring/Ring/Models/MessageModel.swift
@@ -1,7 +1,8 @@
 /*
- *  Copyright (C) 2017-2019 Savoir-faire Linux Inc.
+ *  Copyright (C) 2017-2021 Savoir-faire Linux Inc.
  *
  *  Author: Silbino Gonçalves Matado <silbino.gmatado@savoirfairelinux.com>
+ *  Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,25 +19,128 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
+enum MessageAttributes: String {
+    case interactionId = "id"
+    case type = "type"
+    case invited = "invited"
+    case fileId = "fileId"
+    case displayName = "displayName"
+    case body = "body"
+    case author = "author"
+    case timestamp = "timestamp"
+    case parent = "linearizedParent"
+    case action = "action"
+    case duration = "duration"
+}
+
+enum MessageType: String {
+    case text = "text/plain"
+    case fileTransfer = "application/data-transfer+json"
+    case contact = "member"
+    case call = "application/call-history+json"
+    case location = "location"
+    case merge = "merge"
+    case initial = "initial"
+}
+
+enum ContactAction: String {
+    case add
+    case remove
+    case join
+    case banned
+}
+
 class MessageModel {
 
-    var messageId: Int64 = -1
+    var id: String = ""
+    /// daemonId for dht messages, file transfer id for datatransfer
     var daemonId: String = ""
     var receivedDate: Date = Date()
+    /// message to display for text, call and contact message. File name for swarm data transfer. file name with identifier from photo library for non swarm file transfer
     var content: String = ""
-    var authorURI: String = ""
+    /// jamiId for sender. For outgoing message authorId is empty
+    var authorId: String = ""
     var status: MessageStatus = .unknown
     var transferStatus: DataTransferStatus = .unknown
-    var isGenerated: Bool = false
-    var isTransfer: Bool = false
     var incoming: Bool
-    var isLocationSharing: Bool = false
+    var parentId: String = ""
+    var type: MessageType = .text
 
     init(withId id: String, receivedDate: Date, content: String, authorURI: String, incoming: Bool) {
         self.daemonId = id
         self.receivedDate = receivedDate
         self.content = content
-        self.authorURI = authorURI
+        self.authorId = authorURI
         self.incoming = incoming
+    }
+    // swiftlint:disable:next cyclomatic_complexity
+    init(withInfo info: [String: String], accountJamiId: String) {
+        if let interactionId = info[MessageAttributes.interactionId.rawValue] {
+            self.id = interactionId
+        }
+        if let author = info[MessageAttributes.author.rawValue], author != accountJamiId {
+            self.authorId = author
+        }
+        if let type = info[MessageAttributes.type.rawValue],
+           let messageType = MessageType(rawValue: type) {
+            self.type = messageType
+        }
+        if let content = info[MessageAttributes.body.rawValue], self.type == .text {
+            self.content = content
+        }
+        incoming = !self.authorId.isEmpty
+        if let parent = info[MessageAttributes.parent.rawValue] {
+            self.parentId = parent
+        }
+        if let timestamp = info[MessageAttributes.timestamp.rawValue],
+           let timestampDouble = Double(timestamp) {
+            let receivedDate = Date.init(timeIntervalSince1970: timestampDouble)
+            self.receivedDate = receivedDate
+        }
+        switch self.type {
+        case .text:
+            if let content = info[MessageAttributes.body.rawValue] {
+                self.content = content
+            }
+        case .call:
+            if let duration = info[MessageAttributes.duration.rawValue],
+               let durationDouble = Double(duration) {
+                if durationDouble < 0 {
+                    self.content = self.incoming ? "Incoming call" : "Outgoing call"
+                } else {
+                    let durationSeconds = durationDouble * 0.001
+                    let time = Date.convertSecondsToTimeString(seconds: durationSeconds)
+                    self.content = self.incoming ? "Incoming call - " + time : "Outgoing call - " + time
+                }
+            } else {
+                self.content = self.incoming ? "Missed incoming call" : "Missed outgoing call"
+            }
+        case .contact:
+            if let action = info[MessageAttributes.action.rawValue],
+               let contactAction = ContactAction(rawValue: action) {
+                switch contactAction {
+                case .add:
+                    self.content = self.incoming ? "Invitation received" : "Contact added"
+                case .join:
+                    self.content = "Invitation accepted"
+                case .remove:
+                    self.content = "Contact left conversation"
+                default:
+                    break
+                }
+            }
+        case .fileTransfer:
+            if let fileid = info[MessageAttributes.fileId.rawValue] {
+                self.daemonId = fileid
+            }
+            if let displayName = info[MessageAttributes.displayName.rawValue] {
+                self.content = displayName
+            }
+        case .initial:
+            self.type = .contact
+            self.content = self.incoming ? "Invitation received" : "Contact added"
+        default:
+            break
+        }
     }
 }
