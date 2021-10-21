afa5c8d6bddda388f4871c65525423088797d002
kkostiuk <kateryna.kostiuk@savoirfairelinux.com>
2021-10-21 16:06:19 -0400

    account: add proxy server settings
    Change-Id: I9fc231907bf651d8adef6649e2c03e46d5031502

--- Ring/Ring/Features/Me/Me/MeViewModel.swift
+++ Ring/Ring/Features/Me/Me/MeViewModel.swift
@@ -50,6 +50,7 @@ enum SettingsSection: SectionModelType {
         case jamiUserName(label: String)
         case notifications
         case sipUserName(value: String)
+        case proxy
         case sipPassword(value: String)
         case sipServer(value: String)
         case port(value: String)
@@ -233,9 +234,17 @@ class MeViewModel: ViewModel, Stateable {
     }()
 
     lazy var otherJamiSettings: Observable<SettingsSection> = {
+        var proxyServer = ""
+        if let account = self.accountService.currentAccount,
+           let details = account.details {
+            proxyServer = details.get(withConfigKeyModel: ConfigKeyModel.init(withKey: ConfigKey.proxyServer))
+            self.proxyAddress.accept(proxyServer)
+        }
         let items: [SettingsSection.SectionRow] = [.peerDiscovery,
                                                    .blockedList,
-                                                   .accountState(state: self.accountStatus),
+                                                   .proxy,
+                                                   SettingsSection.SectionRow
+                                                    .accountState(state: self.accountStatus),
                                                    .enableAccount,
                                                    .changePassword,
                                                    .boothMode]
@@ -473,7 +482,6 @@ class MeViewModel: ViewModel, Stateable {
             self.showActionState.accept(.hideLoading)
             return
         }
-
         self.nameService
             .registerNameObservable(withAccount: accountId,
                                     password: password,
@@ -791,6 +799,15 @@ class MeViewModel: ViewModel, Stateable {
               let account = self.accountService.currentAccount else { return }
         self.accountService.enableTurn(enable: enable, accountId: account.id)
         turnEnabled.accept(enable)
+    }
+    let proxyAddress = BehaviorRelay<String>(value: "")
+
+    func changeProxy(proxyServer: String) {
+        guard let accountId = self.accountService.currentAccount?.id else {
+            return
+        }
+        self.accountService.setProxyAddress(accountID: accountId, proxy: proxyServer)
+        self.proxyAddress.accept(proxyServer)
     }
 
     func enableUpnp(enable: Bool) {
