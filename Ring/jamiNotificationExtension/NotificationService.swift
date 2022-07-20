b64bd90349f7d5ac186bd13f1183bb7a500a7b51
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2022-07-20 13:34:13 -0400

    notification: debug presenting notification
    Change-Id: I69fbeba220ef056d9d58eb0c5260bd3180dc1fc2

--- Ring/jamiNotificationExtension/NotificationService.swift
+++ Ring/jamiNotificationExtension/NotificationService.swift
@@ -41,7 +41,7 @@ class NotificationService: UNNotificationServiceExtension {
 
     private static let localNotification = Notification.Name("com.savoirfairelinux.jami.appActive.internal")
 
-    private let notificationTimeout = DispatchTimeInterval.seconds(7)
+    private let notificationTimeout = DispatchTimeInterval.seconds(9)
 
     private let notificationCenter = CFNotificationCenterGetDarwinNotifyCenter()
 
@@ -59,6 +59,7 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         defer {
+            sleep(4)
             finish()
         }
         let requestData = requestToDictionary(request: request)
@@ -134,6 +135,7 @@ class NotificationService: UNNotificationServiceExtension {
                             case .message:
                                 self.presentMessageNotification(from: eventData.jamiId, body: eventData.content)
                             case .fileTransferDone:
+                                os_log("file downloading completed")
                                 if let url = URL(string: eventData.content) {
                                     self.presentFileNotification(from: eventData.jamiId, url: url)
                                 }
@@ -143,6 +145,7 @@ class NotificationService: UNNotificationServiceExtension {
                                 self.syncCompleted = true
                                 self.verifyTasksStatus()
                             case .fileTransferInProgress:
+                                os_log("file downloading started")
                                 self.numberOfFiles += 1
                             case .call:
                                 handleCall(eventData.jamiId, eventData.content)
@@ -356,6 +359,7 @@ extension NotificationService {
     }
 
     private func presentMessageNotification(from: String, body: String) {
+        os_log("present message notification")
         let content = UNMutableNotificationContent()
         content.title = "Incoming message"
         content.subtitle = from
@@ -363,10 +367,19 @@ extension NotificationService {
         content.sound = UNNotificationSound.default
         setNotificationCount(notification: content)
         let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.01, repeats: false)
-        let identifier = Int64(arc4random_uniform(10000000))
-        let notificationRequest = UNNotificationRequest(identifier: "\(identifier)", content: content, trigger: notificationTrigger)
+        let notificationRequest = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: notificationTrigger)
+        UNUserNotificationCenter.current().getNotificationSettings(completionHandler: { (settings) in
+            if settings.authorizationStatus == .notDetermined {
+                os_log("Notification permission notDetermined")
+            } else if settings.authorizationStatus == .denied {
+                os_log("Notification permission denied")
+            } else if settings.authorizationStatus == .authorized {
+                os_log("Notification permission authorized")
+            }
+        })
         UNUserNotificationCenter.current().add(notificationRequest) { (error) in
             if let error = error {
+                os_log("Unable to Add Notification Request %@", "\(error.localizedDescription))")
                 print("Unable to Add Notification Request (\(error), \(error.localizedDescription))")
             }
         }
