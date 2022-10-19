9f461c4a287e62185d54de16f27554910e91ea13
Alireza <mr.toghyiani@icloud.com>
2022-10-19 16:05:33 -0400

    settings: add connectivity settings (TURN)
    Change-Id: If06cc7c74dfe8720ffc3ea3b17311c03928b88e3

--- Ring/Ring/Features/Me/Me/AccountHeader.swift
+++ Ring/Ring/Features/Me/Me/AccountHeader.swift
@@ -18,6 +18,8 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
+import GSKStretchyHeaderView
+
 class AccountHeader: GSKStretchyHeaderView {
 
     @IBOutlet weak var profileImageView: UIImageView!
