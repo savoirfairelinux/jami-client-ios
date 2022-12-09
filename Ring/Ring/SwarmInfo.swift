3cffb52cad38923c4b1531d039f29b2bbf24f193
Binal Ahiya <binal.ahiya@savoirfairelinux.com>
2022-12-09 15:40:59 -0500

    swarm: add more participants in swarm
    This patch implements adding more participant to
    already created swarm via swarm info page
    
    Change-Id: I060d2d92176f634b33c699907d16f549d11a4d98
    GitLab: #196

--- Ring/Ring/SwarmInfo.swift
+++ Ring/Ring/SwarmInfo.swift
@@ -89,6 +89,7 @@ class SwarmInfo {
 
     var avatarHeight: CGFloat = 40
     var avatarSpacing: CGFloat = 2
+    var maximumLimit: Int = 8
     var id: String {
         return conversation?.id ?? ""
     }
@@ -347,7 +348,7 @@ class SwarmInfo {
     }
 
     private func insertAndSortContacts(contacts: [ParticipantInfo]) {
-        var currentValue = self.contacts.value
+        var currentValue = [ParticipantInfo]()
         currentValue.append(contentsOf: contacts)
         self.contacts.accept(currentValue)
     }
