d087ac456c557a7e5e14dacd975d3de7b952af24
Alireza <mr.toghyiani@icloud.com>
2023-08-25 06:40:27 -0400

    Redesign Link Device Form
    Gitlab: #293
    
    Change-Id: I259a7a1121fbe0c1831d6bf6e3c84e9dc0710f42

--- Ring/Ring/Features/Walkthrough/LinkDevice/LinkDeviceViewController.swift
+++ Ring/Ring/Features/Walkthrough/LinkDevice/LinkDeviceViewController.swift
@@ -28,16 +28,16 @@ import SwiftyBeaver
 class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBased {
 
     // MARK: outlets
+    @IBOutlet weak var dismissView: UIView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var cancelButton: DesignableButton!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var linkButton: DesignableButton!
     @IBOutlet weak var containerViewBottomConstraint: NSLayoutConstraint!
-    @IBOutlet weak var pinTextField: DesignableTextField!
-    @IBOutlet weak var passwordTextField: DesignableTextField!
-    @IBOutlet weak var pinInfoButton: UIButton!
+    @IBOutlet weak var pinTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var pinLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var notificationsSwitch: UISwitch!
-    @IBOutlet weak var enableNotificationsLabel: UILabel!
     // MARK: members
     private let disposeBag = DisposeBag()
     var viewModel: LinkDeviceViewModel!
@@ -53,10 +53,10 @@ class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBase
         super.viewDidLoad()
 
         // Style
+        self.contentView.roundTopCorners(radius: 12)
         self.pinTextField.becomeFirstResponder()
         self.configureWalkrhroughNavigationBar()
         self.view.layoutIfNeeded()
-        self.linkButton.applyGradient(with: [UIColor.jamiButtonLight, UIColor.jamiButtonDark], gradient: .horizontal)
         linkButton.titleLabel?.ajustToTextSize()
 
         self.pinTextField.tintColor = UIColor.jamiSecondary
@@ -66,15 +66,17 @@ class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBase
         self.applyL10n()
 
         // bind view model to view
-        self.pinInfoButton.rx.tap
-            .subscribe(onNext: { [weak self] (_) in
-                self?.showPinInfo()
-            })
-            .disposed(by: self.disposeBag)
 
         self.linkButton.rx.tap
             .subscribe(onNext: { [weak self] (_) in
                 self?.viewModel.linkDevice()
+            })
+            .disposed(by: self.disposeBag)
+
+        self.cancelButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.dismiss(animated: true)
             })
             .disposed(by: self.disposeBag)
 
@@ -109,33 +111,35 @@ class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBase
         // bind view to view model
         self.pinTextField.rx.text.orEmpty.bind(to: self.viewModel.pin).disposed(by: self.disposeBag)
         self.passwordTextField.rx.text.orEmpty.bind(to: self.viewModel.password).disposed(by: self.disposeBag)
-        self.notificationsSwitch.rx.isOn.bind(to: self.viewModel.notificationSwitch).disposed(by: self.disposeBag)
 
         // handle keyboard
-        self.adaptToKeyboardState(for: self.scrollView, with: self.disposeBag)
         keyboardDismissTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         NotificationCenter.default.rx.notification(UIDevice.orientationDidChangeNotification)
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] (_) in
                 guard UIDevice.current.portraitOrLandscape else { return }
-                self?.linkButton.updateGradientFrame()
                 self?.configureWalkrhroughNavigationBar()
             })
             .disposed(by: self.disposeBag)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.5) {
+            self.dismissView.alpha = 1
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.dismissView.alpha = 0
+    }
+
     func adaptToSystemColor() {
         view.backgroundColor = UIColor.jamiBackgroundColor
-        scrollView.backgroundColor = UIColor.jamiBackgroundColor
         pinLabel.textColor = UIColor.jamiTextSecondary
         passwordLabel.textColor = UIColor.jamiTextSecondary
-        enableNotificationsLabel.textColor = UIColor.jamiTextSecondary
-        self.pinTextField.backgroundColor = UIColor.jamiBackgroundColor
-        self.passwordTextField.backgroundColor = UIColor.jamiBackgroundColor
-        self.pinTextField.borderColor = UIColor.jamiTextBlue
-        self.passwordTextField.borderColor = UIColor.jamiTextBlue
-        notificationsSwitch.tintColor = UIColor.jamiTextBlue
-        pinInfoButton.tintColor = UIColor.jamiTextBlue
+        linkButton.tintColor = .jamiButtonDark
     }
 
     func setContentInset() {
@@ -152,6 +156,10 @@ class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBase
         }
     }
 
+    func setContentInset(keyboardHeight: CGFloat = 0) {
+        self.containerViewBottomConstraint.constant = keyboardHeight
+    }
+
     @objc
     func dismissKeyboard() {
         self.isKeyboardOpened = false
@@ -160,15 +168,20 @@ class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBase
     }
 
     @objc
-    func keyboardWillAppear(withNotification: NSNotification) {
+    func keyboardWillAppear(withNotification notification: NSNotification) {
         self.isKeyboardOpened = true
-        self.view.addGestureRecognizer(keyboardDismissTapRecognizer)
-        self.setContentInset()
+        self.dismissView.addGestureRecognizer(keyboardDismissTapRecognizer)
+
+        if let userInfo = notification.userInfo,
+           let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            let keyboardHeight = keyboardFrame.size.height
+            self.setContentInset(keyboardHeight: keyboardHeight)
+        }
     }
 
     @objc
     func keyboardWillDisappear(withNotification: NSNotification) {
-        view.removeGestureRecognizer(keyboardDismissTapRecognizer)
+        dismissView.removeGestureRecognizer(keyboardDismissTapRecognizer)
         self.setContentInset()
     }
 
@@ -188,8 +201,7 @@ class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBase
         self.passwordLabel.text = L10n.Global.enterPassword
         self.pinTextField.placeholder = L10n.LinkToAccount.pinPlaceholder
         self.passwordTextField.placeholder = L10n.Global.password
-        self.navigationItem.title = L10n.LinkToAccount.linkButtonTitle
-        self.enableNotificationsLabel.text = self.viewModel.enableNotificationsTitle
+        self.titleLabel.text = L10n.LinkToAccount.linkDeviceTitle
     }
 
     private func showCreationHUD() {
@@ -210,20 +222,5 @@ class LinkDeviceViewController: UIViewController, StoryboardBased, ViewModelBase
                                            preferredStyle: .alert)
         alert.addAction(UIAlertAction.init(title: L10n.Global.ok, style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
-    }
-
-    private func showPinInfo() {
-        if popTip.isVisible {
-            popTip.hide()
-        } else {
-            popTip.shouldDismissOnTap = true
-            popTip.entranceAnimation = .scale
-            popTip.bubbleColor = UIColor.jamiSecondary
-            popTip.textColor = UIColor.white
-            let offset: CGFloat = 20.0
-            popTip.offset = offset - scrollView.contentOffset.y
-            popTip.show(text: L10n.LinkToAccount.explanationPinMessage, direction: .down,
-                        maxWidth: 255, in: self.view, from: pinInfoButton.frame)
-        }
     }
 }
