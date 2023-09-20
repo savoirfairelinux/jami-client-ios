d029b1727d70615d26fc444ae04d98241df95f91
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2023-09-20 15:37:05 -0400

    misc: cleanup
    Change-Id: Ia9f7ed6fc957e70255aeffb38683508560bec442

--- Ring/Ring/Features/Walkthrough/WelcomeViewController.swift
+++ Ring/Ring/Features/Walkthrough/WelcomeViewController.swift
@@ -125,32 +125,7 @@ class WelcomeViewController: UIViewController, StoryboardBased, ViewModelBased {
     }
 
     private func aboutJamiButtonDidTap() {
-        var compileDate: String {
-            let dateDefault = ""
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "YYYYMMdd"
-            let bundleName = Bundle.main.infoDictionary!["CFBundleName"] as? String ?? "Info.plist"
-            if let infoPath = Bundle.main.path(forResource: bundleName, ofType: nil),
-               let infoAttr = try? FileManager.default.attributesOfItem(atPath: infoPath),
-               let infoDate = infoAttr[FileAttributeKey.creationDate] as? Date {
-                return dateFormatter.string(from: infoDate)
-            }
-            return dateDefault
-        }
-
-        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
-
-        let versionName = "Világfa"
-        let alert = UIAlertController(title: "\nJami\nversion: \(appVersion)(\(compileDate))\n\(versionName)", message: "", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: L10n.Global.ok, style: .default, handler: nil))
-        let image = UIImageView(image: UIImage(asset: Asset.jamiIcon))
-        alert.view.addSubview(image)
-        image.translatesAutoresizingMaskIntoConstraints = false
-        alert.view.addConstraint(NSLayoutConstraint(item: image, attribute: .centerX, relatedBy: .equal, toItem: alert.view, attribute: .centerX, multiplier: 1, constant: 0))
-        alert.view.addConstraint(NSLayoutConstraint(item: image, attribute: .centerY, relatedBy: .equal, toItem: alert.view, attribute: .top, multiplier: 1, constant: 0.0))
-        alert.view.addConstraint(NSLayoutConstraint(item: image, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 64.0))
-        alert.view.addConstraint(NSLayoutConstraint(item: image, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 64.0))
-        self.present(alert, animated: true, completion: nil)
+        AppInfoHelper.showAboutJamiAlert(onViewController: self)
     }
 
     func setupButtonActions() {
