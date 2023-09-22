4b2576c39da14f6748b1b4d64bf0e5a783afb209
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2023-09-22 14:47:54 -0400

    smart list: implement a new design
    Change-Id: Ifbdda250b0c831103980c88d068a2d59897952e4

--- Ring/Ring/UI/CustomSearchController.swift
+++ Ring/Ring/UI/CustomSearchController.swift
@@ -18,6 +18,102 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 import UIKit
+import RxSwift
+
+class CustomNavigationBar: UINavigationBar {
+
+    var customHeight: CGFloat = 44.0
+    var customSearchView: UIView?
+    var disposeBag = DisposeBag()
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width, height: customHeight)
+    }
+
+    func addCustomSearchView(with buttons: [UIButton]) {
+        customSearchView = UIView()
+        customSearchView?.translatesAutoresizingMaskIntoConstraints = false
+        customSearchView?.backgroundColor = .clear
+
+        addSubview(customSearchView!)
+        customSearchView?.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        customSearchView?.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        customSearchView?.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        customSearchView?.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+
+        let title = UILabel()
+        title.text = "Search"
+        title.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        customSearchView?.addSubview(title)
+        title.centerXAnchor.constraint(equalTo: customSearchView!.centerXAnchor).isActive = true
+        title.centerYAnchor.constraint(equalTo: customSearchView!.centerYAnchor).isActive = true
+
+        var previousButton: UIButton?
+        for button in buttons {
+            button.translatesAutoresizingMaskIntoConstraints = false
+            customSearchView?.addSubview(button)
+
+            if let prevBtn = previousButton {
+                button.trailingAnchor.constraint(equalTo: prevBtn.leadingAnchor, constant: -15).isActive = true
+            } else {
+                button.trailingAnchor.constraint(equalTo: customSearchView!.trailingAnchor, constant: -15).isActive = true
+            }
+
+            button.centerYAnchor.constraint(equalTo: customSearchView!.centerYAnchor).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 25).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 25).isActive = true
+            previousButton = button
+        }
+    }
+
+    func removeCustomSearchView() {
+        customSearchView?.removeFromSuperview()
+        customSearchView = nil
+        disposeBag = DisposeBag()
+    }
+
+    var increaseHeight = false {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        for subview in self.subviews {
+            let stringFromClass = NSStringFromClass(subview.classForCoder)
+
+            if stringFromClass.contains("UINavigationBarContentView") {
+                if increaseHeight {
+                    subview.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: customHeight)
+                }
+            }
+
+            if stringFromClass.contains("SearchBar") {
+                if increaseHeight {
+                    subview.frame = CGRect(x: 0, y: 40, width: self.frame.width, height: subview.frame.height)
+                }
+            }
+
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: super.intrinsicContentSize.width, height: customHeight)
+    }
+
+    override var frame: CGRect {
+        didSet {
+            var newFrame = frame
+            if newFrame.size.height < customHeight && increaseHeight {
+                newFrame.size.height = customHeight
+            }
+            super.frame = newFrame
+        }
+    }
+}
 
 class CustomSearchController: UISearchController {
     private var customSearchBar = CustomSearchBar()
