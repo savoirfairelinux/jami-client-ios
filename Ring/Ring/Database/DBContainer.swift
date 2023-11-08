44a5ea653cc762bdb8e856c8cd06074977b1b32d
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2023-11-08 10:17:27 -0500

    data base: fix access to connections
    Change-Id: I8fab9e988e92fe303c10ebbbfd6c8851f5e1c717

--- Ring/Ring/Database/DBContainer.swift
+++ Ring/Ring/Database/DBContainer.swift
@@ -54,29 +54,34 @@ final class DBContainer {
     private let dbVersions = [1, 2]
 
     func removeDBForAccount(account: String, removeFolder: Bool) {
+        self.connectionsSemaphore.wait()
         connections[account] = nil
+        self.connectionsSemaphore.signal()
         if !removeFolder { return }
         self.removeAccountFolder(accountId: account)
     }
 
     func removeDBForAccount(account: String) {
+        self.connectionsSemaphore.wait()
         connections[account] = nil
+        self.connectionsSemaphore.signal()
         self.removeDBNamed(dbName: "\(account).db")
     }
 
     func forAccount(account: String) -> Connection? {
+        self.connectionsSemaphore.wait()
+        defer {
+            self.connectionsSemaphore.signal()
+        }
         if connections[account] != nil {
             return connections[account] ?? nil
         }
         guard let dbPath = accountDbPath(accountId: account) else { return nil }
         do {
-            self.connectionsSemaphore.wait()
             connections[account] = try Connection(dbPath)
             connections[account]??.userVersion = dbVersions.last
-            self.connectionsSemaphore.signal()
             return connections[account] ?? nil
         } catch {
-            self.connectionsSemaphore.signal()
             log.error("Unable to open database")
             return nil
         }
