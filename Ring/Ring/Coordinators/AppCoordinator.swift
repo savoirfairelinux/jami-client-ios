cc177a6c3ec2f58c6d58085b8c96fb869ec1df91
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2023-11-25 22:30:05 -0500

    Revert "Avoid showing the initial loading view after welcome form"
    This reverts commit 6fe6d36d9226759ad54485e72eaf7426bdb67bb7.
    
    Change-Id: Ifdec6eb5b1a2d4f2e5e5a9e96a4ee95ec848c6ec

--- Ring/Ring/Coordinators/AppCoordinator.swift
+++ Ring/Ring/Coordinators/AppCoordinator.swift
@@ -102,12 +102,6 @@ final class AppCoordinator: Coordinator, StateableResponsive {
         self.dispatchApplication()
     }
 
-    /// Starts the coordinator
-    func startWithoutLoading() {
-        // ~ Dispatch to the proper screen
-        self.dispatchApplication()
-    }
-
     func migrateAccount(accountId: String) {
         let migratonController = MigrateAccountViewController.instantiate(with: self.injectionBag)
         migratonController.viewModel.accountToMigrate = accountId
@@ -145,16 +139,22 @@ final class AppCoordinator: Coordinator, StateableResponsive {
         let walkthroughCoordinator = WalkthroughCoordinator(with: self.injectionBag)
         walkthroughCoordinator.isAccountFirst = isAccountFirst
         walkthroughCoordinator.withAnimations = animated
-        walkthroughCoordinator.parentCoordinator = self
         walkthroughCoordinator.start()
 
-        self.navigationController.setViewControllers([], animated: false)
         self.addChildCoordinator(childCoordinator: walkthroughCoordinator)
         let walkthroughViewController = walkthroughCoordinator.rootViewController
         self.present(viewController: walkthroughViewController,
                      withStyle: .appear,
                      withAnimation: true,
                      disposeBag: self.disposeBag)
+
+        walkthroughViewController.rx.controllerWasDismissed
+            .subscribe(onNext: { [weak self, weak walkthroughCoordinator] (_) in
+                walkthroughCoordinator?.stateSubject.dispose()
+                self?.removeChildCoordinator(childCoordinator: walkthroughCoordinator)
+                self?.dispatchApplication()
+            })
+            .disposed(by: self.disposeBag)
     }
 
     /// Presents the main interface
