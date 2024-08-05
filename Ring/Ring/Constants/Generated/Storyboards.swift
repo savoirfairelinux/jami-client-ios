1b0cc89283a69713809034c392b8c39181fc7f95
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2024-08-05 13:22:02 -0400

    walkthrough: update account creation
    Gitlab: #397
    Change-Id: Idd69278e6ea722607ad7424cd5580e0146fa6238

--- Ring/Ring/Constants/Generated/Storyboards.swift
+++ Ring/Ring/Constants/Generated/Storyboards.swift
@@ -76,16 +76,6 @@ internal enum StoryboardScene {
 
     internal static let initialScene = InitialSceneType<Ring.ConversationViewController>(storyboard: ConversationViewController.self)
   }
-  internal enum CreateAccountViewController: StoryboardType {
-    internal static let storyboardName = "CreateAccountViewController"
-
-    internal static let initialScene = InitialSceneType<Ring.CreateAccountViewController>(storyboard: CreateAccountViewController.self)
-  }
-  internal enum CreateProfileViewController: StoryboardType {
-    internal static let storyboardName = "CreateProfileViewController"
-
-    internal static let initialScene = InitialSceneType<Ring.CreateProfileViewController>(storyboard: CreateProfileViewController.self)
-  }
   internal enum InitialLoadingViewController: StoryboardType {
     internal static let storyboardName = "InitialLoadingViewController"
 
@@ -97,11 +87,6 @@ internal enum StoryboardScene {
     internal static let storyboardName = "LaunchScreen"
 
     internal static let initialScene = InitialSceneType<UIViewController>(storyboard: LaunchScreen.self)
-  }
-  internal enum LinkDeviceViewController: StoryboardType {
-    internal static let storyboardName = "LinkDeviceViewController"
-
-    internal static let initialScene = InitialSceneType<Ring.LinkDeviceViewController>(storyboard: LinkDeviceViewController.self)
   }
 
   internal enum ScanViewController: StoryboardType {
