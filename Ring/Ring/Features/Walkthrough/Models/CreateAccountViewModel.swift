3be02a43605597552875b2cf4411169b3fb5491a
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2024-08-05 13:22:02 -0400

    walkthrough: update account creation
    Change-Id: Idd69278e6ea722607ad7424cd5580e0146fa6238

---
+++ Ring/Ring/Features/Walkthrough/Models/CreateAccountViewModel.swift
@@ -0,0 +1,242 @@
+/*
+ *  Copyright (C) 2017-2019 Savoir-faire Linux Inc.
+ *
+ *  Author: Thibault Wittemberg <thibault.wittemberg@savoirfairelinux.com>
+ *  Author: Quentin Muret <quentin.muret@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import Foundation
+import RxSwift
+import RxRelay
+import SwiftUI
+
+enum UsernameValidationState {
+    case unknown
+    case available(message: String)
+    case lookingForAvailability(message: String)
+    case invalid(message: String)
+    case unavailable(message: String)
+
+    var isAvailable: Bool {
+        switch self {
+        case .available:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var isDefault: Bool {
+        switch self {
+        case .unknown:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var isVerifying: Bool {
+        switch self {
+        case .lookingForAvailability:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var message: String {
+        switch self {
+        case .unknown:
+            return ""
+        case .available(let message):
+            return message
+        case .lookingForAvailability(let message):
+            return message
+        case .invalid(let message):
+            return message
+        case .unavailable(let message):
+            return message
+        }
+    }
+
+    var textColor: UIColor {
+        switch self {
+        case .unknown:
+            return .clear
+        case .available:
+            return .jamiSuccess
+        case .lookingForAvailability:
+            return .clear
+        case .invalid:
+            return .jamiFailure
+        case .unavailable:
+            return .jamiFailure
+        }
+    }
+}
+
+enum AccountCreationState {
+    case initial
+    case unknown
+    case started
+    case success
+    case nameNotRegistered
+    case timeOut
+    case error(error: AccountCreationError)
+
+    var isInProgress: Bool {
+        switch self {
+        case .started:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var isCompleted: Bool {
+        switch self {
+        case .unknown, .started:
+            return false
+        default:
+            return true
+        }
+    }
+
+    var message: String {
+        switch self {
+        case .error(let error):
+            return error.localizedDescription
+        default:
+            return ""
+        }
+    }
+
+    static func == (lhs: AccountCreationState, rhs: AccountCreationState) -> Bool {
+        switch (lhs, rhs) {
+        case (.unknown, .unknown):
+            return true
+        case (.started, .started):
+            return true
+        case (.success, .success):
+            return true
+        case ( .error, .error):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+enum AccountCreationError: Error {
+    case generic
+    case network
+    case unknown
+    case linkError
+    case wrongCredentials
+}
+
+extension AccountCreationError: LocalizedError {
+
+    var title: String {
+        switch self {
+        case .generic:
+            return L10n.Alerts.accountCannotBeFoundTitle
+        case .network:
+            return L10n.Alerts.accountNoNetworkTitle
+        case .linkError:
+            return L10n.Alerts.accountCannotBeFoundTitle
+        case .wrongCredentials:
+            return L10n.Alerts.errorWrongCredentials
+        default:
+            return L10n.Alerts.accountDefaultErrorTitle
+        }
+    }
+
+    var message: String {
+        switch self {
+        case .generic:
+            return L10n.Alerts.accountDefaultErrorMessage
+        case .network:
+            return L10n.Alerts.accountNoNetworkMessage
+        case .linkError:
+            return L10n.Alerts.accountCannotBeFoundMessage
+        case .wrongCredentials:
+            return ""
+        default:
+            return L10n.Alerts.accountDefaultErrorMessage
+        }
+    }
+}
+
+class CreateAccountViewModel: ObservableObject, ViewModel {
+    @Published var isJoinButtonDisabled = false
+    @Published var usernameValidationState: UsernameValidationState = .unknown
+    @Published var username: String = ""
+
+    private let disposeBag = DisposeBag()
+    private let nameService: NameService
+
+    required init(with injectionBag: InjectionBag) {
+        self.nameService = injectionBag.nameService
+        bindUsernameValidationStatus()
+    }
+
+    private func bindUsernameValidationStatus() {
+        nameService.usernameValidationStatus.asObservable()
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] status in
+                guard let self = self else { return }
+                self.updateValidationState(with: status)
+                self.updateJoinButtonAvailability()
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func updateValidationState(with status: UsernameValidationStatus) {
+        if username.isEmpty {
+            usernameValidationState = .unknown
+            return
+        }
+        switch status {
+        case .lookingUp:
+            usernameValidationState = .lookingForAvailability(message: L10n.CreateAccount.lookingForUsernameAvailability)
+        case .invalid:
+            usernameValidationState = .invalid(message: L10n.CreateAccount.invalidUsername)
+        case .alreadyTaken:
+            usernameValidationState = .unavailable(message: L10n.CreateAccount.usernameAlreadyTaken)
+        case .valid:
+            usernameValidationState = .available(message: L10n.CreateAccount.usernameValid)
+        default:
+            usernameValidationState = .unknown
+        }
+    }
+
+    private func updateJoinButtonAvailability() {
+        isJoinButtonDisabled = !username.isEmpty && !usernameValidationState.isAvailable
+    }
+
+    func usernameUpdated(to newUsername: String) {
+        guard username != newUsername else { return }
+        username = newUsername
+        if username.isEmpty {
+            self.usernameValidationState = .unknown
+            self.updateJoinButtonAvailability()
+        } else {
+            nameService.lookupName(withAccount: "", nameserver: "", name: username)
+        }
+    }
+}
---
+++ Ring/Ring/Features/Walkthrough/Views/Views.swift
@@ -0,0 +1,256 @@
+/*
+ *  Copyright (C) 2024 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import SwiftUI
+
+struct CreateAccountPasswordView: View {
+    @Binding var text: String
+    var placeholder: String
+    var identifier: String = ""
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color(UIColor.secondarySystemGroupedBackground))
+            PasswordFieldView(text: $text, placeholder: placeholder)
+                .padding(.horizontal, 10)
+                .padding(.vertical, 12)
+        }
+    }
+}
+
+struct CreateAccountTextEditView: View {
+    @Binding var text: String
+    var placeholder: String
+    var identifier: String = ""
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color(UIColor.secondarySystemGroupedBackground))
+            TextField(placeholder, text: $text)
+                .padding(.horizontal, 10)
+                .padding(.vertical, 12)
+                .autocorrectionDisabled(true)
+                .autocapitalization(.none)
+                .accessibilityIdentifier(identifier)
+        }
+    }
+}
+
+struct CreateAccountFocusableTextEditView: View {
+    @Binding var text: String
+    @Binding var isTextFieldFocused: Bool
+    var placeholder: String
+    var identifier: String = ""
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color(UIColor.secondarySystemGroupedBackground))
+            FocusableTextField(
+                text: $text,
+                isFirstResponder: $isTextFieldFocused,
+                placeholder: placeholder,
+                identifier: identifier
+            )
+            .padding(.horizontal, 10)
+            .padding(.vertical, 12)
+        }
+    }
+}
+
+struct ScanQRCodeView: View {
+    let width: CGFloat
+    let height: CGFloat
+    var onScan: (String) -> Void
+
+    var body: some View {
+        ZStack {
+            QRCodeScannerView(width: width, height: height, didFindCode: { scannedCode in
+                onScan(scannedCode)
+            })
+            .frame(width: width, height: height)
+            .cornerRadius(12)
+            QRCodeScannerOverlayView(size: (min(width, height) * 0.5))
+                .frame(width: width, height: height)
+        }
+    }
+}
+
+struct QRCodeScannerOverlayView: View {
+    let size: CGFloat
+    var body: some View {
+        GeometryReader { geometry in
+            let frameSize: CGFloat = size
+            let cornerLength: CGFloat = 20
+            let lineWidth: CGFloat = 4
+
+            let rect = CGRect(
+                x: (geometry.size.width - frameSize) / 2,
+                y: (geometry.size.height - frameSize) / 2,
+                width: frameSize,
+                height: frameSize
+            )
+
+            Path { path in
+                // Top-left corner
+                path.move(to: CGPoint(x: rect.minX, y: rect.minY + cornerLength))
+                path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+                path.addLine(to: CGPoint(x: rect.minX + cornerLength, y: rect.minY))
+
+                // Top-right corner
+                path.move(to: CGPoint(x: rect.maxX - cornerLength, y: rect.minY))
+                path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+                path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY + cornerLength))
+
+                // Bottom-left corner
+                path.move(to: CGPoint(x: rect.minX, y: rect.maxY - cornerLength))
+                path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+                path.addLine(to: CGPoint(x: rect.minX + cornerLength, y: rect.maxY))
+
+                // Bottom-right corner
+                path.move(to: CGPoint(x: rect.maxX - cornerLength, y: rect.maxY))
+                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerLength))
+            }
+            .stroke(Color.white, lineWidth: lineWidth)
+        }
+        .background(Color.clear)
+    }
+}
+
+struct QRCodeScannerView: UIViewControllerRepresentable {
+    let width: CGFloat
+    let height: CGFloat
+    var didFindCode: (String) -> Void
+
+    class Coordinator: NSObject, AVCaptureMetadataOutputObjectsDelegate {
+        var parent: QRCodeScannerView
+
+        init(parent: QRCodeScannerView) {
+            self.parent = parent
+        }
+
+        func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+            if let metadataObject = metadataObjects.first {
+                guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+                guard let stringValue = readableObject.stringValue else { return }
+                AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+                parent.didFindCode(stringValue)
+            }
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        let viewController = UIViewController()
+        let session = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            showCameraDisabledMessage(in: viewController)
+            return viewController
+        }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            showCameraDisabledMessage(in: viewController)
+            return viewController
+        }
+
+        if session.canAddInput(videoInput) {
+            session.addInput(videoInput)
+        } else {
+            showCameraDisabledMessage(in: viewController)
+            return viewController
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if session.canAddOutput(metadataOutput) {
+            session.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(context.coordinator, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+            metadataOutput.rectOfInterest = CGRect(x: 0, y: 0, width: width, height: height)
+        } else {
+            showCameraDisabledMessage(in: viewController)
+            return viewController
+        }
+
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = CGRect(x: 0, y: 0, width: width, height: width)
+        previewLayer.videoGravity = .resizeAspectFill
+        viewController.view.layer.addSublayer(previewLayer)
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            session.startRunning()
+        }
+
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+
+    private func showCameraDisabledMessage(in viewController: UIViewController) {
+        viewController.view.backgroundColor = .black
+        let label = UILabel()
+        label.text = L10n.Global.cameraDisabled
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.textAlignment = .center
+        label.frame = CGRect(x: 20, y: 0, width: width - 40, height: height)
+        viewController.view.addSubview(label)
+    }
+}
+
+struct AlertFactory {
+    static func alertWithOkButton(title: String, message: String, action: @escaping () -> Void) -> some View {
+        VStack(spacing: 20) {
+            Text(title)
+                .font(.headline)
+            Text(message)
+            HStack {
+                Spacer()
+                Button(action: action, label: {
+                    Text(L10n.Global.ok)
+                        .foregroundColor(.jamiColor)
+                        .padding(.horizontal)
+                })
+            }
+        }
+    }
+
+    static func createLoadingView() -> some View {
+        VStack(spacing: 20) {
+            Text(L10n.CreateAccount.creatingAccount)
+                .font(.headline)
+                .padding()
+            SwiftUI.ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+                .scaleEffect(2)
+                .padding(.bottom, 30)
+        }
+        .padding()
+    }
+}
