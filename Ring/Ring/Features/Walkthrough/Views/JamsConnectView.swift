da0f0f26b78b3d00e51c41a8e14b2c817aaa341b
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2024-08-05 13:22:02 -0400

    walkthrough: update account creation
    Change-Id: Idd69278e6ea722607ad7424cd5580e0146fa6238

---
+++ Ring/Ring/Features/Walkthrough/Views/JamsConnectView.swift
@@ -0,0 +1,104 @@
+/*
+ *  Copyright (C) 2024 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import SwiftUI
+
+struct JamsConnectView: View {
+    let dismissAction: () -> Void
+    let connectAction: (_ username: String,
+                        _ password: String,
+                        _ server: String) -> Void
+    @SwiftUI.State private var username: String = ""
+    @SwiftUI.State private var password: String = ""
+    @SwiftUI.State private var server: String = ""
+    @SwiftUI.State private var isTextFieldFocused = true
+    var body: some View {
+        VStack {
+            header
+            ScrollView(showsIndicators: false) {
+                Text(L10n.LinkToAccountManager.jamsExplanation)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical)
+                serverView
+                Text(L10n.LinkToAccountManager.enterCredentials)
+                    .padding(.vertical)
+                usernameView
+                passwordView
+            }
+            .padding(.horizontal)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(UIColor.systemGroupedBackground)
+            .ignoresSafeArea()
+        )
+    }
+
+    private var header: some View {
+        ZStack {
+            HStack {
+                cancelButton
+                Spacer()
+                signinButton
+            }
+            Text(L10n.LinkToAccountManager.title)
+                .font(.headline)
+        }
+        .padding()
+    }
+
+    private var cancelButton: some View {
+        Button(action: {
+            dismissAction()
+        }, label: {
+            Text(L10n.Global.cancel)
+                .foregroundColor(Color(UIColor.label))
+        })
+    }
+
+    private var signinButton: some View {
+        Button(action: {
+            connectAction(username, password, server)
+        }, label: {
+            Text(L10n.LinkToAccountManager.signIn)
+                .foregroundColor(signInDisabled ?
+                                 Color(UIColor.secondaryLabel) :
+                        .jamiColor)
+        })
+        .disabled(signInDisabled)
+    }
+
+    private var usernameView: some View {
+        WalkthroughTextEditView(text: $username,
+                                placeholder: L10n.Global.username)
+    }
+
+    private var serverView: some View {
+        let placeholder = L10n.LinkToAccountManager.accountManagerPlaceholder
+        return WalkthroughFocusableTextView(text: $server,
+                                     isTextFieldFocused: $isTextFieldFocused,
+                                     placeholder: placeholder)
+    }
+
+    private var passwordView: some View {
+        WalkthroughPasswordView(text: $password, placeholder: L10n.Global.password)
+    }
+
+    private var signInDisabled: Bool {
+        username.isEmpty || password.isEmpty || server.isEmpty
+    }
+}
---
+++ Ring/Ring/Features/Walkthrough/Views/SIPConfigurationView.swift
@@ -0,0 +1,94 @@
+/*
+ *  Copyright (C) 2024 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import SwiftUI
+
+struct SIPConfigurationView: View {
+    let dismissAction: () -> Void
+    let connectAction: (_ username: String,
+                        _ password: String,
+                        _ server: String) -> Void
+    @SwiftUI.State private var username: String = ""
+    @SwiftUI.State private var password: String = ""
+    @SwiftUI.State private var server: String = ""
+    @SwiftUI.State private var isTextFieldFocused = true
+    var body: some View {
+        VStack {
+            header
+            ScrollView(showsIndicators: false) {
+                Text(L10n.CreateAccount.sipConfigure)
+                    .padding(.vertical)
+                serverView
+                usernameView
+                passwordView
+            }
+            .padding(.horizontal)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(UIColor.systemGroupedBackground)
+            .ignoresSafeArea()
+        )
+    }
+
+    private var header: some View {
+        ZStack {
+            HStack {
+                cancelButton
+                Spacer()
+                configureButton
+            }
+            Text(L10n.Account.sipAccount)
+                .font(.headline)
+        }
+        .padding()
+    }
+
+    private var cancelButton: some View {
+        Button(action: {
+            dismissAction()
+        }, label: {
+            Text(L10n.Global.cancel)
+                .foregroundColor(Color(UIColor.label))
+        })
+    }
+
+    private var configureButton: some View {
+        Button(action: {
+            connectAction(username, password, server)
+        }, label: {
+            Text(L10n.Account.configure)
+                .foregroundColor(.jamiColor)
+        })
+    }
+
+    private var usernameView: some View {
+        WalkthroughTextEditView(text: $username,
+                                placeholder: L10n.Global.username)
+    }
+
+    private var serverView: some View {
+        WalkthroughFocusableTextView(text: $server,
+                                     isTextFieldFocused: $isTextFieldFocused,
+                                     placeholder: L10n.Account.sipServer)
+    }
+
+    private var passwordView: some View {
+        WalkthroughPasswordView(text: $password,
+                                placeholder: L10n.Global.password)
+    }
+}
