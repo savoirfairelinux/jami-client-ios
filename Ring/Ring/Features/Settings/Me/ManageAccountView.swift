eb9ca148b4e23bfaf83753de8100bb7d19eefc5b
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2024-08-12 09:57:56 -0400

    account: add backup
    Change-Id: I727d63ec3ca5c76bebb4d39dd2cb965eee64cc41

--- Ring/Ring/Features/Settings/Me/ManageAccountView.swift
+++ Ring/Ring/Features/Settings/Me/ManageAccountView.swift
@@ -40,6 +40,13 @@ struct ManageAccountView: View {
                             Text(L10n.AccountPage.encryptAccount)
                         }
                     }
+
+                    NavigationLink(destination: BackupAccount()
+                        .background(Color(UIColor.systemGroupedBackground))) {
+                            HStack {
+                                Text("Backup account")
+                            }
+                        }
                 }
 
                 Section {
@@ -110,5 +117,152 @@ struct EditPasswordView: View {
         .onDisappear {
             onDisappearAction()
         }
+    }
+}
+
+
+struct BackupAccount: View {
+    //@StateObject var model: EncryptAccountVM
+
+    private let cornerRadius: CGFloat = 10
+    private let shadowRadius: CGFloat = 1
+    private let horizontalPadding: CGFloat = 10
+    private let verticalPadding: CGFloat = 12
+    @SwiftUI.State private var showPicker = false
+    @SwiftUI.State private var filePath: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    @SwiftUI.State private var name: String = "account.gz"
+    @SwiftUI.State private var showAlert = false
+    @SwiftUI.State private var password = ""
+    @SwiftUI.State private var server: String = ""
+    @SwiftUI.State private var isTextFieldFocused = true
+
+
+    //    init(account: AccountModel, accountService: AccountsService) {
+    //        _model = StateObject(wrappedValue: EncryptAccountVM(account: account, accountService: accountService))
+    //    }
+
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Your Jami account is registered only on this device as an archive containing the keys of your account. Access to this archive can be protected by a password.")
+                backupButton()
+                Spacer()
+            }
+
+            if showAlert {
+                encryptViewAlert()
+            }
+        }
+        .sheet(isPresented: $showPicker) {
+            DocumentPicker(fileURL: $filePath)
+        }
+        .onChange(of: filePath) { _ in
+            withAnimation {
+                isTextFieldFocused = true
+                showAlert = true
+            }
+            //saveLogTo(path: filePath)
+        }
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(UIColor.systemGroupedBackground).ignoresSafeArea())
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("Backup account")
+    }
+
+    func saveLogTo() {
+       // let filename = "name"
+        guard filePath.startAccessingSecurityScopedResource() else {
+            //showSaveError()
+            return
+        }
+        print("export to \(filePath.absoluteString)")
+//        let finalUrl = path.appendingPathComponent(filename)
+//        do {
+////            try self.systemService.currentLog.write(to: finalUrl, atomically: true, encoding: String.Encoding.utf8)
+//        } catch {
+//            path.stopAccessingSecurityScopedResource()
+//           // showSaveError()
+//            return
+//        }
+        filePath.stopAccessingSecurityScopedResource()
+    }
+
+    @ViewBuilder
+    func encryptViewAlert() -> some View {
+        CustomAlert(content: { encryptView() })
+    }
+
+    func encryptView() -> some View {
+        VStack(spacing: 20) {
+            Text("backup account")
+                .font(.headline)
+            Text("archive will be saved to \(filePath.absoluteString)")
+                .font(.subheadline)
+            WalkthroughFocusableTextView(text: $name,
+                                         isTextFieldFocused: $isTextFieldFocused,
+                                         placeholder: "file name")
+            passwordFieldsSection()
+            actionButtons()
+        }
+    }
+
+    @ViewBuilder
+    private func passwordFieldsSection() -> some View {
+        VStack {
+            PasswordFieldView(text: $password,
+                              placeholder: L10n.Global.enterPassword)
+            .textFieldStyleInAlert()
+        }
+    }
+
+    @ViewBuilder
+    private func actionButtons() -> some View {
+        HStack {
+            Button(action: {
+//                showEncryptView = false
+//                if !passwordValidated {
+//                    resetPasswords()
+//                }
+            }, label: {
+                Text(L10n.Global.cancel)
+                    .foregroundColor(.jamiColor)
+            })
+
+            Spacer()
+
+            Button(action: {
+                saveLogTo()
+//                showEncryptView = false
+//                withAnimation {
+//                    encryptionEnabled = true
+//                }
+            }, label: {
+                Text("backup")
+                    .foregroundColor(.jamiColor)
+            })
+//            .disabled(!passwordValidated)
+//            .opacity(passwordValidated ? 1 : 0.5)
+        }
+    }
+
+    func backupButton() -> some View {
+        Button(action: {
+            withAnimation {
+                showPicker = true
+            }
+        }, label: {
+            Text("Backup account")
+                .foregroundColor(Color(UIColor.label))
+                .padding(.horizontal, 10)
+                .padding(.vertical, 12)
+                .frame(maxWidth: .infinity)
+                .background(Color.jamiTertiaryControl)
+                .cornerRadius(10)
+        })
+//        .listRowBackground(Color.clear)
+//        .optionalRowSeparator(hidden: true)
+//        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+//        .listRowBackground(Color.clear)
     }
 }
