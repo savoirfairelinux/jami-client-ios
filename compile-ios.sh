343c1ef53f0ea3e79155feaf3e4e7f7492ceb2bf
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2024-12-01 16:51:31 -0500

    iOS: build arm64 simulator
    Change-Id: Ie578d0f715d69cfa1db45aea5a2450c8c0e7e432

--- compile-ios.sh
+++ compile-ios.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 
 export BUILDFORIOS=1
 export MIN_IOS_VERSION=14.5
@@ -15,56 +15,41 @@ do
       HOST="${1#--host=}"
     ;;
     --release)
-    RELEASE=1
+      RELEASE=1
     ;;
   esac
   shift
 done
 
-if test -z "$HOST"
-then
-  if [ "$IOS_TARGET_PLATFORM" = "all" ]
-  then
-    ARCHS=("arm64" "x86_64")
-  elif [ "$IOS_TARGET_PLATFORM" = "iPhoneSimulator" ]
-  then
-    ARCHS=("x86_64")
-  elif [ "$IOS_TARGET_PLATFORM" = "iPhoneOS" ]
-  then
-    ARCHS=("arm64")
-  fi
+if [ "$IOS_TARGET_PLATFORM" = "all" ]; then
+  PLATFORMS=("iPhoneOS" "iPhoneSimulator")
+  ARCHS_IOS=("arm64")
+  ARCHS_SIMULATOR=("x86_64" "arm64")
 else
-  ARCHS=("${HOST%%-*}")
-  case "$HOST" in
-    aarch64-*)
-      IOS_TARGET_PLATFORM="iPhoneOS"
-      ARCHS=("arm64")
-    ;;
-    x86_64-*)
-      IOS_TARGET_PLATFORM="iPhoneSimulator"
-      ARCHS=("x86_64")
-    ;;
-  esac
+  PLATFORMS=("$IOS_TARGET_PLATFORM")
+  if [ "$IOS_TARGET_PLATFORM" = "iPhoneOS" ]; then
+    ARCHS=("arm64")
+  elif [ "$IOS_TARGET_PLATFORM" = "iPhoneSimulator" ]; then
+    ARCHS=("x86_64" "arm64")
+  fi
 fi
 
-IOS_TOP_DIR="$(pwd)"
+if [ -z "$NPROC" ]; then
+  NPROC=$(sysctl -n hw.ncpu || echo -n 1)
+fi
 
 if [ -z "$DAEMON_DIR" ]; then
   DAEMON_DIR="$(pwd)/../daemon"
-  echo "DAEMON_DIR not provided trying to find it in $DAEMON_DIR"
+  echo "DAEMON_DIR not provided, trying to find it in $DAEMON_DIR"
 fi
 if [ ! -d "$DAEMON_DIR" ]; then
-  echo 'Daemon not found.'
-  echo 'If you cloned the daemon in a custom location override' \
-       'use DAEMON_DIR to point to it'
-  echo "You can also use our meta repo which contains both:
-        https://review.jami.net/admin/repos/jami-project"
+  echo "Daemon not found."
+  echo "If you cloned the daemon in a custom location, override it using DAEMON_DIR to point to it."
   exit 1
 fi
 
-if [ ! $(which gas-preprocessor.pl) ]
-then
-  echo 'gas-preprocessor.pl not found. Trying to install...'
+if [ ! $(which gas-preprocessor.pl) ]; then
+  echo "gas-preprocessor.pl not found. Trying to install..."
   mkdir -p "$DAEMON_DIR/extras/tools/build/bin/"
   (curl -L https://github.com/libav/gas-preprocessor/raw/master/gas-preprocessor.pl \
     -o "$DAEMON_DIR/extras/tools/build/bin/gas-preprocessor.pl" \
@@ -72,139 +57,178 @@ then
   export PATH="$DAEMON_DIR/extras/tools/build/bin:$PATH"
 fi
 
-if [ -z "$NPROC"  ]; then
-  NPROC=$(sysctl -n hw.ncpu || echo -n 1)
-fi
+IOS_TOP_DIR="$(pwd)"
+FAT_DIR="$IOS_TOP_DIR/fat"
+mkdir -p "$FAT_DIR/lib"
 
-export IOS_TARGET_PLATFORM
-echo "Building for $IOS_TARGET_PLATFORM for $ARCHS"
-
-cd "$DAEMON_DIR"
-
-for ARCH in "${ARCHS[@]}"
-do
-  mkdir -p "contrib/native-$ARCH"
-  cd "contrib/native-$ARCH"
-
-  if [ "$ARCH" = "arm64" ]
-  then
-    HOST=aarch64-apple-darwin_ios
-    IOS_TARGET_PLATFORM="iPhoneOS"
+for PLATFORM in "${PLATFORMS[@]}"; do
+  if [ "$PLATFORM" = "iPhoneOS" ]; then
+    ARCHS=("${ARCHS_IOS[@]}")
   else
-    HOST="$ARCH"-apple-darwin_ios
-    IOS_TARGET_PLATFORM="iPhoneSimulator"
+    ARCHS=("${ARCHS_SIMULATOR[@]}")
   fi
-  export IOS_TARGET_PLATFORM
-
-  SDKROOT=$(xcode-select -print-path)/Platforms/${IOS_TARGET_PLATFORM}.platform/Developer/SDKs/${IOS_TARGET_PLATFORM}${SDK_VERSION}.sdk
 
   host=$(sw_vers -productVersion)
   if [ "12.0" \> "$host" ]
   then
-      SDK="$(echo "print '${IOS_TARGET_PLATFORM}'.lower()" | python)"
+      SDK="$(echo "print '${PLATFORM}'.lower()" | python)"
   else
-      SDK="$(echo "print('${IOS_TARGET_PLATFORM}'.lower())" | python3)"
+      SDK="$(echo "print('${PLATFORM}'.lower())" | python3)"
   fi
 
-  CC="xcrun -sdk $SDK clang"
-  CXX="xcrun -sdk $SDK clang++"
+  IOS_TARGET_PLATFORM=$PLATFORM
+  export IOS_TARGET_PLATFORM
 
-  SDKROOT="$SDKROOT" ../bootstrap --host="$HOST" --disable-libav --disable-plugin --disable-libarchive --enable-ffmpeg
 
-  echo "Building contrib"
-  make fetch
-  make -j"$NPROC" || exit 1
+  for ARCH in "${ARCHS[@]}"; do
+    echo "Building for $PLATFORM $ARCH"
+    BUILD_DIR="$DAEMON_DIR/contrib/native-$PLATFORM-$ARCH"
+    mkdir -p "$BUILD_DIR"
+    cd "$BUILD_DIR"
 
-  cd ../..
-  echo "Building daemon"
+    if [ "$ARCH" = "arm64" ] && [ "$PLATFORM" = "iPhoneOS" ]; then
+      HOST=aarch64-apple-darwin
+    elif [ "$ARCH" = "arm64" ] && [ "$PLATFORM" = "iPhoneSimulator" ]; then
+      HOST=aarch64-apple-darwin
+    else
+      HOST="$ARCH"-apple-darwin
+    fi
 
-  CFLAGS="-arch $ARCH -isysroot $SDKROOT"
-  if [ "$IOS_TARGET_PLATFORM" = "iPhoneOS" ]
-  then
-    CFLAGS+=" -miphoneos-version-min=$MIN_IOS_VERSION -fembed-bitcode"
-  else
-    CFLAGS+=" -mios-simulator-version-min=$MIN_IOS_VERSION"
-  fi
+    echo "HOST: $HOST"
 
-  if [ "$RELEASE" = "1" ]
-  then
-    CFLAGS+=" -O3"
-  fi
+    # Set SDKROOT dynamically based on PLATFORM
+    SDKROOT=$(xcode-select -print-path)/Platforms/$PLATFORM.platform/Developer/SDKs/$PLATFORM${SDK_VERSION}.sdk
 
-  CXXFLAGS="-stdlib=libc++ -std=c++17 $CFLAGS"
-  LDFLAGS="$CFLAGS"
+    # Validate SDKROOT
+    if [ ! -d "$SDKROOT" ]; then
+      echo "Error: SDKROOT does not exist: $SDKROOT"
+      exit 1
+    fi
+    echo "*****"
+    echo $SDK
+    echo "SDKROOT: $SDKROOT"
 
-  ./autogen.sh || exit 1
-  mkdir -p "build-ios-$ARCH"
-  cd "build-ios-$ARCH"
+    # Define compilers and flags
+    CC="xcrun -sdk $SDK clang"
+    CXX="xcrun -sdk $SDK clang++"
 
-  JAMI_CONF="--host=$HOST \
-             --without-dbus \
-             --disable-plugin \
-             --disable-libarchive \
-             --enable-static \
-             --without-natpmp \
-             --disable-shared \
-             --prefix=$IOS_TOP_DIR/DEPS/$ARCH"
+    echo "Running bootstrap for $PLATFORM $ARCH"
+    SDKROOT="$SDKROOT" ../bootstrap --host="$HOST" \
+                                     --disable-libav \
+                                     --disable-plugin \
+                                     --disable-libarchive \
+                                     --enable-ffmpeg
 
-  if [ "$RELEASE" = "0" ]
-  then
-    JAMI_CONF+=" --enable-debug"
-  fi
+    echo "Building contrib"
+    CURRENT_DIR="$(pwd)"
+    echo $CURRENT_DIR
+    make fetch
+    make -j"$NPROC" || exit 1
 
-  "$DAEMON_DIR"/configure $JAMI_CONF \
-                        CC="$CC $CFLAGS" \
-                        CXX="$CXX $CXXFLAGS" \
-                        OBJCXX="$CXX $CXXFLAGS" \
-                        LD="$LD" \
-                        CFLAGS="$CFLAGS" \
-                        CXXFLAGS="$CXXFLAGS" \
-                        LDFLAGS="$LDFLAGS" || exit 1
+    cd "$DAEMON_DIR"
 
-  # We need to copy this file or else it's just an empty file
-  rsync -a "$DAEMON_DIR/src/buildinfo.cpp" ./src/buildinfo.cpp
+    CFLAGS="-arch $ARCH -isysroot $SDKROOT"
+    CXXFLAGS="$CFLAGS -stdlib=libc++"
+    LDFLAGS="-arch $ARCH -isysroot $SDKROOT -L$BUILD_DIR/lib"
+    echo "Building daemon for $PLATFORM $ARCH"
 
-  make -j"$NPROC" || exit 1
-  make install || exit 1
+    CFLAGS="-arch $ARCH -isysroot $SDKROOT"
+    if [ "$PLATFORM" = "iPhoneOS" ]; then
+      CFLAGS+=" -miphoneos-version-min=$MIN_IOS_VERSION -fembed-bitcode"
+    else
+      CFLAGS+=" -mios-simulator-version-min=$MIN_IOS_VERSION"
+    fi
 
-  rsync -ar "$DAEMON_DIR/contrib/$HOST/lib/"*.a "$IOS_TOP_DIR/DEPS/$ARCH/lib/"
-  # copy headers for extension
-  rsync -ar "$DAEMON_DIR/contrib/$HOST/include/opendht" "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  rsync -ar $DAEMON_DIR/contrib/$HOST/include/msgpack.hpp "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  rsync -ar $DAEMON_DIR/contrib/$HOST/include/gnutls "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  rsync -ar $DAEMON_DIR/contrib/$HOST/include/json "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  rsync -ar $DAEMON_DIR/contrib/$HOST/include/msgpack "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  rsync -ar $DAEMON_DIR/contrib/$HOST/include/yaml-cpp "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  rsync -ar $DAEMON_DIR/contrib/$HOST/include/libavutil "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  rsync -ar $DAEMON_DIR/contrib/$HOST/include/fmt "$IOS_TOP_DIR/DEPS/$ARCH/include/"
-  cd "$IOS_TOP_DIR/DEPS/$ARCH/lib/"
-  for i in *.a ; do mv "$i" "${i/-$HOST.a/.a}" ; done
+    if [ "$RELEASE" = "1" ]; then
+      CFLAGS+=" -O3"
+    fi
 
-  cd "$DAEMON_DIR"
+    CXXFLAGS="-stdlib=libc++ -std=c++17 $CFLAGS"
+    LDFLAGS="$CFLAGS"
+
+    ./autogen.sh || exit 1
+    BUILD_ARCH_DIR="$DAEMON_DIR/build-$PLATFORM-$ARCH"
+    mkdir -p "$BUILD_ARCH_DIR"
+    cd "$BUILD_ARCH_DIR"
+
+    JAMI_CONF="--host=$HOST \
+               --without-dbus \
+               --disable-plugin \
+               --disable-libarchive \
+               --enable-static \
+               --without-natpmp \
+               --disable-shared \
+               --prefix=$IOS_TOP_DIR/DEPS/$PLATFORM/$ARCH"
+
+    if [ "$RELEASE" = "0" ]; then
+      JAMI_CONF+=" --enable-debug"
+    fi
+
+    "$DAEMON_DIR"/configure $JAMI_CONF \
+                            CC="$CC $CFLAGS" \
+                            CXX="$CXX $CXXFLAGS" \
+                            OBJCXX="$CXX $CXXFLAGS" \
+                            LD="$LD" \
+                            CFLAGS="$CFLAGS" \
+                            CXXFLAGS="$CXXFLAGS" \
+                            LDFLAGS="$LDFLAGS" || exit 1
+
+    make -j"$NPROC" || exit 1
+    make install || exit 1
+
+    rsync -ar "$DAEMON_DIR/contrib/$HOST/lib/"*.a "$IOS_TOP_DIR/DEPS/$PLATFORM/$ARCH/lib/"
+    rsync -ar "$DAEMON_DIR/contrib/$HOST/include/" "$IOS_TOP_DIR/DEPS/$PLATFORM/$ARCH/include/"
+
+    cd "$IOS_TOP_DIR"
+  done
 done
 
-cd "$IOS_TOP_DIR"
-
-FAT_DIR="$IOS_TOP_DIR/fat"
-mkdir -p "$FAT_DIR"
-
-if ((${#ARCHS[@]} == "2"))
-then
-  mkdir -p "$FAT_DIR/lib"
-  echo "Making fat lib for ${ARCHS[0]} and ${ARCHS[1]}"
-  LIBFILES="$IOS_TOP_DIR/DEPS/${ARCHS[0]}/lib/"*.a
-  for f in $LIBFILES
-  do
-    libFile=${f##*/}
-    echo "Processing $libFile lib..."
-    #There is only 2 ARCH max... So let's make it simple
-    lipo -create  "$IOS_TOP_DIR/DEPS/${ARCHS[0]}/lib/$libFile"  \
-                  "$IOS_TOP_DIR/DEPS/${ARCHS[1]}/lib/$libFile" \
-                  -output "$FAT_DIR/lib/$libFile"
-  done
-else
-  echo "No need for fat lib"
-  rsync -ar --delete "$IOS_TOP_DIR/DEPS/${ARCHS[0]}/lib/"*.a "$FAT_DIR/lib"
+echo "Creating fat libraries"
+LIBFILES=""
+if [ -d "$IOS_TOP_DIR/DEPS/iPhoneSimulator/arm64/lib" ]; then
+  LIBFILES="$IOS_TOP_DIR/DEPS/iPhoneSimulator/arm64/lib/"*.a
+elif [ -d "$IOS_TOP_DIR/DEPS/iPhoneSimulator/x86_64/lib" ]; then
+  LIBFILES="$IOS_TOP_DIR/DEPS/iPhoneSimulator/x86_64/lib/"*.a
+elif [ -d "$IOS_TOP_DIR/DEPS/iPhoneOS/arm64/lib" ]; then
+  LIBFILES="$IOS_TOP_DIR/DEPS/iPhoneOS/arm64/lib/"*.a
 fi
 
-rsync -ar --delete "$IOS_TOP_DIR/DEPS/${ARCHS[0]}/include/"* "$FAT_DIR/include"
+if [ -z "$LIBFILES" ]; then
+  echo "No libraries found to create fat libraries. Exiting."
+  exit 1
+fi
+
+for libFile in $LIBFILES; do
+  lib=$(basename "$libFile")
+  echo "Processing $lib"
+
+  lipoInputs=()
+  if [ -f "$IOS_TOP_DIR/DEPS/iPhoneOS/arm64/lib/$lib" ]; then
+    lipoInputs+=("$IOS_TOP_DIR/DEPS/iPhoneOS/arm64/lib/$lib")
+  fi
+  if [ -f "$IOS_TOP_DIR/DEPS/iPhoneSimulator/arm64/lib/$lib" ]; then
+    lipoInputs+=("$IOS_TOP_DIR/DEPS/iPhoneSimulator/arm64/lib/$lib")
+  fi
+  if [ -f "$IOS_TOP_DIR/DEPS/iPhoneSimulator/x86_64/lib/$lib" ]; then
+    lipoInputs+=("$IOS_TOP_DIR/DEPS/iPhoneSimulator/x86_64/lib/$lib")
+  fi
+
+  if [ ${#lipoInputs[@]} -eq 0 ]; then
+    echo "Warning: No architectures found for $lib. Skipping."
+    continue
+  fi
+
+  echo "Creating fat library for $lib"
+  lipo -create "${lipoInputs[@]}" -output "$FAT_DIR/lib/$lib"
+done
+
+if [ -d "$IOS_TOP_DIR/DEPS/iPhoneOS/arm64/include/" ]; then
+  rsync -ar "$IOS_TOP_DIR/DEPS/iPhoneOS/arm64/include/" "$FAT_DIR/include/"
+elif [ -d "$IOS_TOP_DIR/DEPS/iPhoneSimulator/arm64/include/" ]; then
+  rsync -ar "$IOS_TOP_DIR/DEPS/iPhoneSimulator/arm64/include/" "$FAT_DIR/include/"
+elif [ -d "$IOS_TOP_DIR/DEPS/iPhoneSimulator/x86_64/include/" ]; then
+  rsync -ar "$IOS_TOP_DIR/DEPS/iPhoneSimulator/x86_64/include/" "$FAT_DIR/include/"
+else
+  echo "No headers found to copy. Exiting."
+  exit 1
+fi
