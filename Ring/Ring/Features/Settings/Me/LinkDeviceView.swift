f2b64509e8d1c3f0f1d362a5061dfea01a6c8f1b
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2025-01-07 15:04:51 -0500

    Link device
    Change-Id: Ia8d71c8351620247b1695fb4a3cce6798f55100b

--- Ring/Ring/Features/Settings/Me/LinkDeviceView.swift
+++ Ring/Ring/Features/Settings/Me/LinkDeviceView.swift
@@ -18,134 +18,220 @@
 
 import SwiftUI
 
+enum EntryMode: String, CaseIterable, Identifiable {
+    case qrCode = "Scan QR Code"
+    case label = "Enter PIN Code"
+
+    var id: String { rawValue }
+}
+
 struct LinkDeviceView: View {
     @ObservedObject var model: LinkedDevicesVM
     @SwiftUI.State var askForPassword: Bool
     @SwiftUI.State var password: String = ""
+    @SwiftUI.State private var entryMode: EntryMode = .qrCode
+    @Environment(\.verticalSizeClass)
+    var verticalSizeClass
 
     var body: some View {
-        CustomAlert(content: { createLinkDeviceView() })
+        createLinkDeviceView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(UIColor.systemGroupedBackground)
+                            .ignoresSafeArea())
     }
 
+    @ViewBuilder
     func createLinkDeviceView() -> some View {
-        VStack(spacing: 20) {
-            if askForPassword {
-                passwordView()
+        switch model.exportState {
+        case .initial:
+            initialView
+        case .connecting:
+            connectingView()
+        case .authenticating(peerAddress: let peerAddress):
+            authenticatedView(address: peerAddress ?? "")
+        case .inProgress:
+            loadingView()
+        case .error(let error):
+            errorView(error)
+        case .success:
+            successView()
+        }
+    }
+
+    private var initialView: some View {
+        Group {
+            if verticalSizeClass == .regular {
+                portraitView
             } else {
-                switch model.generatingState {
-                case .initial, .generatingPin:
-                    loadingView()
-                case .success(let pin):
-                    successView(pin: pin)
-                case .error(let error):
-                    errorView(error: error.description)
-                }
+                landscapeView
             }
         }
+    }
+
+    private var portraitView: some View {
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 30) {
+                info
+                tokenView
+                Spacer()
+            }
+            .padding()
+        }
+    }
+
+    private var landscapeView: some View {
+        HStack(spacing: 30) {
+            VStack {
+                Spacer().frame(height: 30)
+                info
+                Spacer()
+            }
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 30) {
+                    tokenView
+                }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+
+    private var info: some View {
+        (
+            Text("On the new device, initiate a new account.\n")
+                + Text("Select Add Account > Connect from another device.\n").bold()
+                + Text(entryMode == .qrCode ? "When ready, scan the QR Code" : "When ready, enter the code and press `Connect`.")
+        )
+        .multilineTextAlignment(.center)
+        .frame(maxWidth: 500)
+    }
+
+    @ViewBuilder private var tokenView: some View {
+        Group {
+            Picker("Entry Mode", selection: $entryMode) {
+                ForEach(EntryMode.allCases) { mode in
+                    Text(mode.rawValue).tag(mode)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            if entryMode == .label {
+                tokenEntry
+            } else {
+                qrCodeView
+            }
+        }
+        Spacer()
+    }
+
+    private func connectingView() -> some View {
+        VStack(spacing: 30) {
+            info
+            SwiftUI.ProgressView()
+                .padding()
+            Spacer()
+        }
+        .padding()
+    }
+
+    func authenticatedView(address: String) -> some View {
+        VStack {
+            Text("New device found at address below. Is that you?\nClicking on confirm will continue transfering account")
+                .multilineTextAlignment(.center)
+            Text("new device IP: \(address)").bold()
+                .multilineTextAlignment(.center)
+            HStack {
+                Spacer()
+                Button(action: {
+                    model.confirmAddDevice()
+                }, label: {
+                    Text("Confirm")
+                        .foregroundColor(Color(UIColor.label))
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 12)
+                        .background(Color.jamiTertiaryControl)
+                        .cornerRadius(10)
+                })
+                .padding()
+                Button(action: {
+                    model.cancelAddDevice()
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(Color(UIColor.label))
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 12)
+                        .background(Color.jamiTertiaryControl)
+                        .cornerRadius(10)
+                })
+                Spacer()
+            }
+        }
+        .padding()
     }
 
     func loadingView() -> some View {
         VStack {
-            SwiftUI.ProgressView(L10n.AccountPage.generatingPin)
+            SwiftUI.ProgressView()
                 .padding()
         }
-        .frame(minWidth: 280, minHeight: 150)
     }
 
-    func passwordView() -> some View {
-        VStack(spacing: 20) {
-            Text(L10n.LinkDevice.title)
-                .font(.headline)
-            Text(L10n.AccountPage.passwordForPin)
-                .font(.subheadline)
-            PasswordFieldView(text: $password, placeholder: L10n.Global.enterPassword)
-                .textFieldStyleInAlert()
-            HStack {
-                Button(action: {
-                    withAnimation {
-                        model.showLinkDeviceAlert = false
-                    }
-                }, label: {
-                    Text(L10n.Global.cancel)
-                        .foregroundColor(.jamiColor)
-                })
-                Spacer()
-                Button(action: {
-                    withAnimation {
-                        askForPassword = false
-                    }
-                    model.linkDevice(with: password)
-                }, label: {
-                    Text(L10n.LinkToAccount.linkButtonTitle)
-                        .foregroundColor(.jamiColor)
-                })
-                .disabled(password.isEmpty)
-                .opacity(password.isEmpty ? 0.5 : 1)
-            }
-        }
-    }
-
-    func successView(pin: String) -> some View {
-        VStack(spacing: 20) {
-            Text("\(pin)")
-                .foregroundColor(.jamiColor)
-                .font(.headline)
-                .conditionalTextSelection()
-            if let image = model.PINImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 140, height: 140)
-            }
-            HStack(spacing: 15) {
-                Image(systemName: "info.circle")
-                    .resizable()
-                    .foregroundColor(.jamiColor)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 20, height: 20)
-                VStack(spacing: 5) {
-                    Text(L10n.AccountPage.pinExplanationTitle)
-                    Text(L10n.AccountPage.pinExplanationMessage)
-                        .font(.footnote)
-                }
-            }
-            .padding()
-            .background(Color.jamiTertiaryControl)
-            .cornerRadius(12)
-
-            HStack {
-                Spacer()
-                Button(action: {
-                    withAnimation {
-                        model.showLinkDeviceAlert = false
-                    }
-                }, label: {
-                    Text(L10n.Global.close)
-                        .foregroundColor(.jamiColor)
-                        .padding(.horizontal)
-                })
-            }
-        }
-    }
-
-    func errorView(error: String) -> some View {
+    private func successView() -> some View {
         VStack {
-            Text(L10n.AccountPage.pinError + ": \(error.description)")
-                .foregroundColor(Color(UIColor.jamiFailure))
-                .font(.subheadline)
+            Image(systemName: "checkmark.circle.fill")
+                .foregroundColor(Color(UIColor.jamiSuccess))
+                .font(.system(size: 50))
                 .padding()
-            HStack {
-                Spacer()
-                Button(action: {
-                    withAnimation {
-                        model.showLinkDeviceAlert = false
-                    }
-                }, label: {
-                    Text(L10n.Global.close)
-                        .foregroundColor(.jamiColor)
-                        .padding(.horizontal)
-                })
+            Text("You are all set!")
+            Text("Your account is successfully imported on the new device!")
+            Spacer()
+        }
+    }
+
+    private func errorView(_ message: String) -> some View {
+        VStack {
+            Image(systemName: "exclamationmark.circle.fill")
+                .foregroundColor(Color(UIColor.jamiFailure))
+                .font(.system(size: 50))
+                .padding()
+            Text(message)
+            Spacer()
+        }
+    }
+
+    private var qrCodeView: some View {
+        ScanQRCodeView(width: 350, height: 280) { jamiAuthentication in
+            model.handleAuthenticationUri(jamiAuthentication)
+        }
+    }
+
+    func connectButton() -> some View {
+        Button(action: {
+            withAnimation {
+                model.handleAuthenticationUri(model.exportToken)
             }
+        }, label: {
+            Text("Connect")
+                .foregroundColor(Color(UIColor.label))
+                .padding(.horizontal, 10)
+                .padding(.vertical, 12)
+                .background(Color.jamiTertiaryControl)
+                .cornerRadius(10)
+        })
+    }
+
+    private var tokenEntry: some View {
+        VStack {
+            WalkthroughTextEditView(text: $model.exportToken,
+                                    placeholder: "token")
+                .onAppear {
+                    model.entryError = nil
+                }
+            Text(model.entryError ?? "error")
+                .font(.footnote)
+                .foregroundColor(model.entryError != nil ? Color(UIColor.jamiFailure) : .clear)
+            connectButton()
+                .padding()
         }
     }
 }
