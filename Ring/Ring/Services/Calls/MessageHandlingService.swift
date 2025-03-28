3143cd7a91770ae212eb99d1b23b907ee4b1f275
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2025-03-28 14:01:52 -0400

    Swarm call: update call service
    Change-Id: Ia11001313a176fce890bfeab14fc4ff729c75d48

---
+++ Ring/Ring/Services/Calls/MessageHandlingService.swift
@@ -0,0 +1,163 @@
+/*
+ *  Copyright (C) 2017-2025 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import RxSwift
+import RxRelay
+
+// MARK: - Protocol Definitions
+
+protocol MessageHandling: VCardSender {
+    func sendInCallMessage(callID: String, message: String, accountId: AccountModel)
+}
+
+// MARK: - MessageHandlingService
+
+final class MessageHandlingService: MessageHandling {
+    
+    // MARK: - Private Constants
+    
+    private enum Constants {
+        static let ringVCardMIMETypePrefix = "x-ring/ring.profile.vcard;"
+        static let messagePrefix = "text/plain"
+    }
+    
+    // MARK: - Dependencies
+    
+    private let callsAdapter: CallsAdapter
+    private let dbManager: DBManager
+    private let calls: BehaviorRelay<[String: CallModel]>
+    private let newMessagesStream: PublishSubject<ServiceEvent>
+    
+    // MARK: - Initialization
+    
+    init(
+        callsAdapter: CallsAdapter,
+        dbManager: DBManager,
+        calls: BehaviorRelay<[String: CallModel]>,
+        newMessagesStream: PublishSubject<ServiceEvent>
+    ) {
+        self.callsAdapter = callsAdapter
+        self.dbManager = dbManager
+        self.calls = calls
+        self.newMessagesStream = newMessagesStream
+    }
+    
+    // MARK: - VCardSender Protocol Implementation
+    
+    func sendVCard(callID: String, accountID: String) {
+        guard !accountID.isEmpty, !callID.isEmpty else { return }
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let self = self, 
+                  let profile = self.dbManager.accountVCard(for: accountID) else { return }
+            
+            let jamiId = profile.uri
+            VCardUtils.sendVCard(card: profile,
+                               callID: callID,
+                               accountID: accountID,
+                               sender: self, 
+                               from: jamiId)
+        }
+    }
+    
+    // MARK: - MessageHandling Protocol Implementation
+    
+    func sendInCallMessage(callID: String, message: String, accountId: AccountModel) {
+        guard let call = calls.value[callID] else { return }
+        
+        let messageDictionary = [Constants.messagePrefix: message]
+        callsAdapter.sendTextMessage(withCallID: callID,
+                                   accountId: accountId.id,
+                                   message: messageDictionary,
+                                   from: call.paricipantHash(),
+                                   isMixed: true)
+        
+        notifyOutgoingMessage(message: message, call: call, accountId: accountId)
+    }
+    
+    func sendChunk(callID: String, message: [String: String], accountId: String, from: String) {
+        callsAdapter.sendTextMessage(withCallID: callID,
+                                   accountId: accountId,
+                                   message: message,
+                                   from: from,
+                                   isMixed: true)
+    }
+    
+    // MARK: - Message Handling
+    
+    func handleIncomingMessage(callId: String, fromURI: String, message: [String: String]) {
+        guard let call = calls.value[callId] else { return }
+        
+        if isVCardMessage(message) {
+            handleVCardMessage(fromURI: fromURI, call: call, message: message)
+            return
+        }
+        
+        notifyIncomingMessage(fromURI: fromURI, call: call, messageContent: message.values.first)
+    }
+    
+    // MARK: - Private Helpers
+    
+    private func isVCardMessage(_ message: [String: String]) -> Bool {
+        return message.keys.contains { $0.hasPrefix(Constants.ringVCardMIMETypePrefix) }
+    }
+    
+    private func handleVCardMessage(fromURI: String, call: CallModel, message: [String: String]) {
+        var data = [String: Any]()
+        data[ProfileNotificationsKeys.ringID.rawValue] = fromURI
+        data[ProfileNotificationsKeys.accountId.rawValue] = call.accountId
+        data[ProfileNotificationsKeys.message.rawValue] = message
+        
+        NotificationCenter.default.post(
+            name: NSNotification.Name(ProfileNotifications.messageReceived.rawValue),
+            object: nil,
+            userInfo: data
+        )
+    }
+    
+    private func notifyOutgoingMessage(message: String, call: CallModel, accountId: AccountModel) {
+        let accountHelper = AccountModelHelper(withAccount: accountId)
+        let type = accountHelper.isAccountSip() ? URIType.sip : URIType.ring
+        
+        let contactUri = JamiURI(schema: type, infoHash: call.participantUri, account: accountId)
+        guard let stringUri = contactUri.uriString, let uri = accountHelper.uri else { return }
+        
+        var event = ServiceEvent(withEventType: .newOutgoingMessage)
+        event.addEventInput(.content, value: message)
+        event.addEventInput(.peerUri, value: stringUri)
+        event.addEventInput(.accountId, value: accountId.id)
+        event.addEventInput(.accountUri, value: uri)
+        
+        newMessagesStream.onNext(event)
+    }
+    
+    private func notifyIncomingMessage(fromURI: String, call: CallModel, messageContent: String?) {
+        let accountId = call.accountId
+        let displayName = call.displayName
+        let registeredName = call.registeredName
+        let name = !displayName.isEmpty ? displayName : registeredName
+        
+        var event = ServiceEvent(withEventType: .newIncomingMessage)
+        event.addEventInput(.content, value: messageContent)
+        event.addEventInput(.peerUri, value: fromURI.filterOutHost())
+        event.addEventInput(.name, value: name)
+        event.addEventInput(.accountId, value: accountId)
+        
+        newMessagesStream.onNext(event)
+    }
+}
---
+++ Ring/RingTests/TestableModels/ObjCMockCallsAdapter.m
@@ -0,0 +1,130 @@
+/*
+ *  Copyright (C) 2025-2025 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#import "ObjCMockCallsAdapter.h"
+
+@implementation ObjCMockCallsAdapter
+
+- (nullable NSDictionary<NSString *, NSString *> *)callDetailsWithCallId:(NSString *)callId accountId:(NSString *)accountId {
+    self.callDetailsCallCount++;
+    self.callDetailsCallId = callId;
+    self.callDetailsAccountId = accountId;
+    return self.callDetailsReturnValue;
+}
+
+- (nullable NSArray<NSDictionary<NSString *, NSString *> *> *)currentMediaListWithCallId:(NSString *)callId accountId:(NSString *)accountId {
+    self.currentMediaListCallCount++;
+    self.currentMediaListCallId = callId;
+    self.currentMediaListAccountId = accountId;
+    return self.currentMediaListReturnValue;
+}
+
+- (void)answerMediaChangeResquest:(NSString *)callId accountId:(NSString *)accountId withMedia:(NSArray *)mediaList {
+    self.answerMediaChangeResquestCallCount++;
+    self.answerMediaChangeResquestCallId = callId;
+    self.answerMediaChangeResquestAccountId = accountId;
+    self.answerMediaChangeResquestMedia = mediaList;
+}
+
+- (void)sendTextMessageWithCallID:(NSString*)callId accountId:(NSString*)accountId message:(NSDictionary*)message from:(NSString*)jamiId isMixed:(bool)isMixed {
+    self.sendTextMessageCalled = YES;
+    self.sentTextMessageCallId = callId;
+    self.sentTextMessageAccountId = accountId;
+    self.sentTextMessageMessage = message;
+    self.sentTextMessageFrom = jamiId;
+    self.sentTextMessageIsMixed = isMixed;
+}
+
+// Conference Management Method Implementations
+- (NSArray<NSString*>*)getConferenceCalls:(NSString*)conferenceId accountId:(NSString*)accountId {
+    self.getConferenceCallsCallCount++;
+    self.getConferenceCallsConferenceId = conferenceId;
+    self.getConferenceCallsAccountId = accountId;
+    return self.getConferenceCallsReturnValue ?: @[];
+}
+
+- (NSArray*)getConferenceInfo:(NSString*)conferenceId accountId:(NSString*)accountId {
+    self.getConferenceInfoCallCount++;
+    self.getConferenceInfoConferenceId = conferenceId;
+    self.getConferenceInfoAccountId = accountId;
+    return self.getConferenceInfoReturnValue ?: @[];
+}
+
+- (NSDictionary<NSString*, NSString*>*)getConferenceDetails:(NSString*)conferenceId accountId:(NSString*)accountId {
+    self.getConferenceDetailsCallCount++;
+    self.getConferenceDetailsConferenceId = conferenceId;
+    self.getConferenceDetailsAccountId = accountId;
+    return self.getConferenceDetailsReturnValue ?: @{};
+}
+
+- (BOOL)joinConference:(NSString*)confID call:(NSString*)callID accountId:(NSString*)accountId account2Id:(NSString*)account2Id {
+    self.joinConferenceCallCount++;
+    self.joinConferenceConferenceId = confID;
+    self.joinConferenceCallId = callID;
+    self.joinConferenceAccountId = accountId;
+    self.joinConferenceAccount2Id = account2Id;
+    return YES;
+}
+
+- (BOOL)joinConferences:(NSString*)firstConf secondConference:(NSString*)secondConf accountId:(NSString*)accountId account2Id:(NSString*)account2Id {
+    self.joinConferencesCallCount++;
+    self.joinConferencesConferenceId = firstConf;
+    self.joinConferencesSecondConferenceId = secondConf;
+    self.joinConferencesAccountId = accountId;
+    self.joinConferencesAccount2Id = account2Id;
+    return YES;
+}
+
+- (BOOL)joinCall:(NSString*)firstCall second:(NSString*)secondCall accountId:(NSString*)accountId account2Id:(NSString*)account2Id {
+    self.joinCallCallCount++;
+    self.joinCallFirstCallId = firstCall;
+    self.joinCallSecondCallId = secondCall;
+    self.joinCallAccountId = accountId;
+    self.joinCallAccount2Id = account2Id;
+    return YES;
+}
+
+- (BOOL)hangUpCall:(NSString*)callId accountId:(NSString*)accountId {
+    self.hangUpCallCallCount++;
+    self.hangUpCallCallId = callId;
+    self.hangUpCallAccountId = accountId;
+    return self.hangUpCallReturnValue;
+}
+
+- (BOOL)hangUpConference:(NSString*)conferenceId accountId:(NSString*)accountId {
+    self.hangUpConferenceCallCount++;
+    self.hangUpConferenceCallId = conferenceId;
+    self.hangUpConferenceAccountId = accountId;
+    return self.hangUpConferenceReturnValue;
+}
+
+- (void)setActiveParticipant:(NSString*)callId forConference:(NSString*)conferenceId accountId:(NSString*)accountId {
+    self.setActiveParticipantCallCount++;
+    self.setActiveParticipantJamiId = callId;
+    self.setActiveParticipantConferenceId = conferenceId;
+    self.setActiveParticipantAccountId = accountId;
+}
+
+- (void)setConferenceLayout:(int)layout forConference:(NSString*)conferenceId accountId:(NSString*)accountId {
+    self.setConferenceLayoutCallCount++;
+    self.setConferenceLayoutLayout = layout;
+    self.setConferenceLayoutConferenceId = conferenceId;
+    self.setConferenceLayoutAccountId = accountId;
+}
+
+@end
