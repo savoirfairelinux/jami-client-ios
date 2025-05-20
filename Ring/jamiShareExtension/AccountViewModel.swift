bee8e8b88b69e0443ca9712a145bf3482e985ed7
rawaha.elhoussayni@savoirfairelinux.com <rawaha.elhoussayni@savoirfairelinux.com>
2025-05-20 11:41:11 -0400

    files: share extension
    Change-Id: I49f20fc5d6ed49043e4765fedebd0115479d5fca

---
+++ Ring/jamiShareExtension/AccountViewModel.swift
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2025-2025 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import RxSwift
+
+class AccountViewModel: ObservableObject, Identifiable, Equatable {
+    let id: String
+    @Published var name: String
+    @Published var avatar: String
+
+    private let adapterService: AdapterService
+    private let disposeBag = DisposeBag()
+
+    init(id: String, adapterService: AdapterService, initialName: String = "", initialAvatar: String = "") {
+        self.id = id
+        self.adapterService = adapterService
+        self.name = initialName
+        self.avatar = initialAvatar
+        fetchAccountDetails()
+    }
+
+    static func == (lhs: AccountViewModel, rhs: AccountViewModel) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    private func fetchAccountDetails() {
+        adapterService.resolveLocalAccountDetails(accountId: id)
+            .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))  
+            .observe(on: MainScheduler.instance)                                
+            .subscribe(onSuccess: { [weak self] details in
+                self?.name = details["accountName"] ?? self?.name ?? ""
+                self?.avatar = details["accountAvatar"] ?? self?.avatar ?? ""
+            })
+            .disposed(by: disposeBag)
+    }
+}
---
+++ Ring/jamiShareExtension/ShareViewController.swift
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (C) 2025-2025 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+import UIKit
+import SwiftUI
+import Social
+import RxSwift
+
+@objc class ShareViewController: UIViewController {
+    private var viewModel: ShareViewModel!  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let sharedItems = extensionContext?.inputItems as? [NSExtensionItem] ?? []
+        viewModel = ShareViewModel(sharedItems: sharedItems)  
+
+        let hostingController = UIHostingController(rootView:
+            ShareView(
+                items: sharedItems,
+                viewModel: viewModel,
+                closeAction: { [weak self] in
+                    self?.viewModel.closeShareExtension()
+                    self?.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+                }
+            )
+        )
+
+        addChild(hostingController)
+        hostingController.view.frame = view.bounds
+        view.addSubview(hostingController.view)
+        hostingController.didMove(toParent: self)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        viewModel.closeShareExtension()
+    }
+}
