595bfa4137a726ad529749e4f7a41696c5297583
rawaha.elhoussayni@savoirfairelinux.com <rawaha.elhoussayni@savoirfairelinux.com>
2025-05-20 11:41:11 -0400

    files: share extension
    Change-Id: I49f20fc5d6ed49043e4765fedebd0115479d5fca

---
+++ Ring/jamiShareExtension/ShareViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+import SwiftUI
+import Social
+import RxSwift
+
+@objc class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let sharedItems = extensionContext?.inputItems as? [NSExtensionItem] ?? []
+        let viewModel = ShareViewModel(sharedItems: sharedItems)
+
+        let hostingController = UIHostingController(rootView:
+                                                        ShareView(
+                                                            items: sharedItems,
+                                                            viewModel: viewModel,
+                                                            closeAction: { [weak self] in
+                                                                self?.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+                                                                print("======CLOSEDSHEET") // For manual close
+                                                            }
+                                                        )
+        )
+
+        addChild(hostingController)
+        hostingController.view.frame = view.bounds
+        view.addSubview(hostingController.view)
+        hostingController.didMove(toParent: self)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("======CLOSEDSHEET") // For swipe-down dismiss
+    }
+}
