798c4a70a3fc5addbcc6e8a62d76464c2d42cef2
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2025-06-20 09:16:40 -0400

    files: share extension
    Change-Id: I49f20fc5d6ed49043e4765fedebd0115479d5fca
    https://git.jami.net/savoirfairelinux/jami-client-ios/-/issues/381

---
+++ Ring/jamiShareExtension/Adapter.h
@@ -0,0 +1,158 @@
+/*
+ *  Copyright (C) 2025-2025 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#import <Foundation/Foundation.h>
+
+
+
+typedef NS_ENUM(int, MessageStatus)  {
+    MessageStatusUnknown = 0,
+    MessageStatusSending,
+    MessageStatusSent,
+    MessageStatusDisplayed,
+    MessageStatusFailure,
+    MessageStatusCanceled
+};
+
+typedef NS_ENUM(NSInteger, LookupNameState) {
+    LookupNameStateFound = 0,
+    LookupNameStateInvalidName,
+    LookupNameStateNotFound,
+    LookupNameStateError
+};
+
+@interface LookupNameResponse : NSObject
+
+@property (nonatomic, retain) NSString* accountId;
+@property (nonatomic) LookupNameState state;
+@property (nonatomic, retain) NSString* address;
+@property (nonatomic, retain) NSString* name;
+@property (nonatomic, retain) NSString* requestedName;
+
+@end
+
+
+typedef NS_ENUM(UInt32, NSDataTransferEventCode)  {
+    invalid = 0,
+    created,
+    unsupported,
+    wait_peer_acceptance,
+    wait_host_acceptance,
+    ongoing,
+    finished,
+    closed_by_host,
+    closed_by_peer,
+    invalid_pathname,
+    unjoinable_peer
+};
+
+typedef NS_ENUM(UInt32, NSDataTransferError)  {
+    success = 0,
+    unknown,
+    io,
+    invalid_argument
+};
+
+typedef NS_ENUM(UInt32, NSDataTransferFlags)  {
+    direction = 0 
+};
+
+
+@interface NSDataTransferInfo: NSObject
+{
+@public
+    NSString* accountId;
+    NSDataTransferEventCode lastEvent;
+    UInt32 flags;
+    int64_t totalSize;
+    int64_t bytesProgress;
+    NSString* peer;
+    NSString* displayName;
+    NSString* path;
+    NSString* mimetype;
+    NSString* conversationId;
+}
+@property (strong, nonatomic) NSString* accountId;
+@property (nonatomic) NSDataTransferEventCode lastEvent;
+@property (nonatomic) UInt32 flags;
+@property (nonatomic) int64_t totalSize;
+@property (nonatomic) int64_t bytesProgress;
+@property (strong, nonatomic) NSString* peer;
+@property (strong, nonatomic) NSString* displayName;
+@property (strong, nonatomic) NSString* path;
+@property (strong, nonatomic) NSString* conversationId;
+@property (strong, nonatomic) NSString* mimetype;
+@end
+
+
+@interface SwarmMessageWrap : NSObject
+
+@property (nonatomic, strong) NSString* id;
+@property (nonatomic, strong) NSString* type;
+@property (nonatomic, strong) NSString* linearizedParent;
+@property (nonatomic, strong) NSDictionary<NSString *, NSString *>* body;
+@property (nonatomic, strong) NSArray<NSDictionary<NSString *, NSString *> *>* reactions;
+@property (nonatomic, strong) NSArray<NSDictionary<NSString *, NSString *> *>* editions;
+@property (nonatomic, strong) NSDictionary<NSString *, NSNumber* >* status;
+
+@end
+
+
+@protocol AdapterDelegate;
+
+
+
+@interface Adapter: NSObject
+
+@property (class, nonatomic, weak) id <AdapterDelegate> delegate;
+
+
++ (id<AdapterDelegate>)delegate;
++ (void)setDelegate:(id<AdapterDelegate>)delegate;
+
+
+- (BOOL)initDaemon;
+- (BOOL)startDaemon;
+
+- (NSArray *)getAccountList;
+- (void)setAccountActive:(NSString *)accountID
+                  active:(bool)active;
+
+
+- (NSArray*)getSwarmConversationsForAccount:(NSString*) accountId;
+- (void)sendSwarmMessage:(NSString*)accountId conversationId:(NSString*)conversationId message:(NSString*)message parentId:(NSString*)parentId flag:(int32_t)flag;
+- (void)sendSwarmFileWithName:(NSString*)displayName
+                    accountId:(NSString*)accountId
+               conversationId:(NSString*)conversationId
+                 withFilePath:(NSString*)filePath
+                       parent:(NSString*)parent;
+
+- (NSDataTransferError) dataTransferInfoWithId:(NSString*)fileId
+                                          accountId:(NSString*)accountId
+                                           withInfo:(NSDataTransferInfo*)info;
+
+- (void)lookupAddressWithAccount:(NSString*)account nameserver:(NSString*)nameserver
+                         address:(NSString*)address;
+
+- (NSDictionary *)getAccountDetails:(NSString *)accountID;
+
+- (NSMutableDictionary<NSString*,NSString*>*)getConversationInfoForAccount:(NSString*) accountId conversationId:(NSString*) conversationId;
+
+- (NSArray<NSDictionary<NSString*,NSString*>*>*)getConversationMembers:(NSString*) accountId conversationId:(NSString*) conversationId;
+
+@end
