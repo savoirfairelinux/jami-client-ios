e3e881b88d19694b659fef4053405556de411942
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2025-08-18 15:39:54 -0400

    Share extension: accesibility
    Change-Id: If62a660e1b0bf802d84e2b6781992cdbb33bc551

--- Ring/Ring/Constants/Generated/Strings.swift
+++ Ring/Ring/Constants/Generated/Strings.swift
@@ -854,21 +854,21 @@ internal enum L10n {
   internal enum Global {
     /// Accept
     internal static let accept = L10n.tr("Localizable", "global.accept", fallback: "Accept")
-    /// *  Copyright (C) 2017-2025 Savoir-faire Linux Inc.
+    /// * Copyright (C) 2017-2025 Savoir-faire Linux Inc.
     ///  *
-    ///  *  This program is free software; you can redistribute it and/or modify
-    ///  *  it under the terms of the GNU General Public License as published by
-    ///  *  the Free Software Foundation; either version 3 of the License, or
-    ///  *  (at your option) any later version.
+    ///  * This program is free software; you can redistribute it and/or modify
+    ///  * it under the terms of the GNU General Public License as published by
+    ///  * the Free Software Foundation; either version 3 of the License, or
+    ///  * (at your option) any later version.
     ///  *
-    ///  *  This program is distributed in the hope that it will be useful,
-    ///  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ///  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ///  *  GNU General Public License for more details.
+    ///  * This program is distributed in the hope that it will be useful,
+    ///  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ///  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ///  * GNU General Public License for more details.
     ///  *
-    ///  *  You should have received a copy of the GNU General Public License
-    ///  *  along with this program; if not, write to the Free Software
-    ///  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+    ///  * You should have received a copy of the GNU General Public License
+    ///  * along with this program; if not, write to the Free Software
+    ///  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
     internal static let accountSettings = L10n.tr("Localizable", "global.accountSettings", fallback: "Account Settings")
     /// Block
     internal static let block = L10n.tr("Localizable", "global.block", fallback: "Block")
