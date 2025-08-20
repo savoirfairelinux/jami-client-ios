7a69732c12f5105c357e6d377a4421892388326e
ovari123 <ovari123@zoho.com>
2025-08-20 04:04:13 -0400

    homogenize variable name: answer → accept
    Follow up to the https://review.jami.net/c/jami-client-qt/+/31410 patch.
    
    Change-Id: I80cc09c43493e7eb487fc35d1578825e4f4a1a3e

--- Ring/RingUITests/AccountCreationTest.swift
+++ Ring/RingUITests/AccountCreationTest.swift
@@ -1,21 +1,19 @@
 /*
- *  Copyright (C) 2024 Savoir-faire Linux Inc.
+ * Copyright (C) 2024-2025 Savoir-faire Linux Inc.
  *
- *  Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
 import XCTest
@@ -116,7 +114,7 @@ final class AccountCreationTest: JamiBaseNoAccountUITest {
 
         // Verify the state of the "Cancel" button
         let cancelButton = app.buttons[AccessibilityIdentifiers.cancelCreatingAccount]
-        XCTAssertTrue(cancelButton.isEnabled, "Cancel button is not enabled")
+        XCTAssertTrue(cancelButton.isEnabled, "Cancel button is disabled")
         cancelButton.tap()
 
         // Verify that account creation view dismissed
@@ -206,7 +204,7 @@ final class AccountCreationTest: JamiBaseNoAccountUITest {
         // Verify the error text
         let label = app.staticTexts[AccessibilityIdentifiers.createAccountErrorLabel]
         let expectedText = L10n.CreateAccount.usernameAlreadyTaken
-        XCTAssertEqual(label.label, expectedText, "Explanation label is not correct")
+        XCTAssertEqual(label.label, expectedText, "Explanation label is incorrect")
 
         // Verify the state of the "Join" button
         XCTAssertFalse(joinButton.isEnabled, "The Join button is enabled")
