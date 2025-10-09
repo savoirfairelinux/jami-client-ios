81916b0ad7711e2d46a568c0be9ac37ef8c83f16
ovari123 <ovari123@zoho.com>
2025-10-09 01:23:13 -0400

    About: cleanup
    GitLab: https://git.jami.net/savoirfairelinux/jami-client-android/-/issues/1911
    
    Change-Id: I699206f20a48858a2c1557cca9ef69975a40b2c6

--- Ring/Ring/Constants/Constants.swift
+++ Ring/Ring/Constants/Constants.swift
@@ -17,11 +17,15 @@
  */
 
 let contributorsDevelopers: String = """
+Abhishek Ojha
 Adrien Béraud
+Alassane Yattara
 Albert Babí
 Alexander Lussier-Cullen
 Alexandr Sergheev
+Alexandre Eberhardt
 Alexandre Lision
+Alexandre Savard
 Alexandre Viau
 Aline Bonnet
 Aline Gondim Santos
@@ -29,21 +33,24 @@ Alireza Toghiani
 Amin Bandali
 AmirHossein Naghshzan
 Amna Snene
+Andreas Hatziiliou
 Andreas Traczyk
 Anthony Léonard
 Brando Tovar
 Capucine Berthet
 Charles-Francis Damedey
 Christophe Villemer
-Charlotte Hoffmann
 Cyrille Béraud
 Dorina Mosku
 Eden Abitbol
-Edric Milaret
+Édric Milaret
 Éloi Bail
 Emma Falkiewitz
 Emmanuel Lepage-Vallée
+Emmanuel Milou
+Eric Bjarstal
 Fadi Shehadeh
+Félix Boucher
 Franck Laurent
 François-Simon Fauteux-Chapleau
 Frédéric Guimont
@@ -51,33 +58,47 @@ Guillaume Heller
 Guillaume Roguez
 Hadrien De Sousa
 Hugo Lefeuvre
+Hussein Abdallah
+Ilyas Erdogan
+Io Daza-Dillon
+Jérôme Lamy
 Julien Grossholtz
+Julien Robert
 Kateryna Kostiuk
-Kessler Dupont-Teevin
+Kessler DuPont-Teevin
+Lanius-collaris
+Larbi Gharib
 Léo Banno-Cloutier
+Léopold Chappuis
 Liam Courdoson
 Loïc Siret
-Marianne Forget
+Louis Maillard
 Mathéo Joseph
+Maxim Cournoyer
 Michel Schmit
 Mingrui Zhang
-Mohamed Chibani
 Mohamed Amine Younes Bouacida
+Mohamed Chibani
+Nghia Dam
 Nicolas Jäger
 Nicolas Reynaud
 Nicolas Vengeon
 Olivier Gregoire
 Olivier Soldano
+Page Magnier-Slimani
 Patrick Keroulas
+Pavan Koushik Nellore
 Peymane Marandi
 Philippe Gorley
 Pierre Duchemin
 Pierre Lespagnol
 Pierre Nicolas
 Raphaël Brulé
+Rawaha El Houssayni
 Rayan Osseiran
 Romain Bertozzi
 Saher Azer
+Samuel Kayode
 Sébastien Blin
 Seva Ivanov
 Silbino Gonçalves Matado
@@ -87,12 +108,14 @@ Stepan Salenikovich
 Thibault Wittemberg
 Thomas Ballasi
 Trevor Tabah
+Vitalii Nikitchyn
 Vsevolod Ivanov
 Xavier Jouslin de Noray
 Yang Wang
+Ziwei Wang
 """
 
-let contributorsArts: String = """
+let contributorsMedia: String = """
 Charlotte Hoffman
 Marianne Forget
 """
