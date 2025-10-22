f2bad85399cef43a16752496e34f384f33dcc5d3
ovari123 <ovari123@zoho.com>
2025-10-22 16:37:01 -0400

    homogenize variable name: ban → block
    ban → block
    banned → blocked
    
    Follow up to the https://review.jami.net/c/jami-client-ios/+/32098 patch.
    
    Change-Id: I3b18bec329d54afd757d82630cad6887d57b2d5b

--- Ring/Ring/SwarmInfo/SwarmInfoVM.swift
+++ Ring/Ring/SwarmInfo/SwarmInfoVM.swift
@@ -1,14 +1,16 @@
 /*
- * Copyright (C) 2022 - 2025 Savoir-faire Linux Inc. *
+ * Copyright (C) 2022-2025 Savoir-faire Linux Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version. *
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details. *
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -305,7 +307,7 @@ class SwarmInfoVM: ObservableObject {
            let participant = conversation.getParticipants().first {
             self.contactsService
                 .removeContact(withId: participant.jamiId,
-                               ban: false,
+                               block: false,
                                withAccountId: accountId)
                 .asObservable()
                 .subscribe(onCompleted: {})
@@ -333,7 +335,7 @@ class SwarmInfoVM: ObservableObject {
 
         contactsService
             .removeContact(withId: participantId,
-                           ban: true,
+                           block: true,
                            withAccountId: accountId)
             .asObservable()
             .subscribe(onCompleted: {})
