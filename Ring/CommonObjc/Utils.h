3a5b3e33100c48a6c9d3ee9e02e1905eacebe902
ovari123 <ovari123@zoho.com>
2025-10-30 00:32:13 -0400

    fix typo: dictionary/dictionaries
    arrayOfDictionnarisToVectorOfMap → arrayOfDictionariesToVectorOfMap
    mapToDictionnary → mapToDictionary
    mapToDictionnaryWithInt → mapToDictionaryWithInt
    NSDictionary → NSDictionary
    resDictionnary → resDictionary
    
    Change-Id: I21d6f1c87794df64b94dd10bc7174c158501a1df

--- Ring/CommonObjc/Utils.h
+++ Ring/CommonObjc/Utils.h
@@ -1,21 +1,19 @@
 /*
- *  Copyright (C) 2016-2019 Savoir-faire Linux Inc.
+ * Copyright (C) 2016-2025 Savoir-faire Linux Inc.
  *
- *  Author: Edric Ladent-Milaret <edric.ladent-milaret@savoirfairelinux.com>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
  * USA.
  */
 
@@ -28,13 +26,13 @@
 @interface Utils : NSObject
 
 + (NSArray*)vectorToArray:(const std::vector<std::string>&)vector;
-+ (NSMutableDictionary*)mapToDictionnary:
++ (NSMutableDictionary*)mapToDictionary:
     (const std::map<std::string, std::string>&)map;
-+ (std::map<std::string, std::string>)dictionnaryToMap:(NSDictionary*)dict;
++ (std::map<std::string, std::string>)dictionaryToMap:(NSDictionary*)dict;
 + (NSArray*)vectorOfMapsToArray:(const std::vector<std::map<std::string, std::string>>&)vectorOfMaps;
 + (NSData*)dataFromVectorOfUInt8:(std::vector<uint8_t>)vectorOfUInt8;
 + (std::vector<uint8_t>)vectorOfUInt8FromData:(NSData*)data;
-+ (std::vector<std::map<std::string, std::string>>)arrayOfDictionnarisToVectorOfMap:(NSArray*)dictionaries;
-+ (NSMutableDictionary*)mapToDictionnaryWithInt:(const std::map<std::string, int32_t>&)map;
++ (std::vector<std::map<std::string, std::string>>)arrayOfDictionariesToVectorOfMap:(NSArray*)dictionaries;
++ (NSMutableDictionary*)mapToDictionaryWithInt:(const std::map<std::string, int32_t>&)map;
 
 @end
