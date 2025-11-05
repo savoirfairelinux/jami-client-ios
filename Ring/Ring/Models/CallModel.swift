a5748ac2b89697616d67f10f7caee11a83d068c6
ovari123 <ovari123@zoho.com>
2025-11-04 22:12:00 -0400

    CallModel.swift: update unhold to resume
    GitLab: https://git.jami.net/savoirfairelinux/jami-daemon/-/issues/1154
    Change-Id: I9d623b6b06787647f7a5c5faae808325895c6b90

--- Ring/Ring/Models/CallModel.swift
+++ Ring/Ring/Models/CallModel.swift
@@ -1,22 +1,19 @@
 /*
- *  Copyright (C) 2017-2019 Savoir-faire Linux Inc.
+ * Copyright (C) 2017-2025 Savoir-faire Linux Inc.
  *
- *  Author: Silbino Gonçalves Matado <silbino.gmatado@savoirfairelinux.com>
- *  Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
 enum CallState: String {
@@ -28,7 +25,7 @@ enum CallState: String {
     case busy = "BUSY"
     case failure = "FAILURE"
     case hold = "HOLD"
-    case unhold = "UNHOLD"
+    case resume = "RESUME"
     case inactive = "INACTIVE"
     case over = "OVER"
     case unknown = "UNKNOWN"
@@ -53,7 +50,7 @@ enum CallState: String {
     }
 
     func isActive() -> Bool {
-        return self == .incoming || self == .connecting || self == .ringing || self == .current || self == .hold || self == .unhold
+        return self == .incoming || self == .connecting || self == .ringing || self == .current || self == .hold || self == .resume
     }
 }
 
@@ -267,7 +264,7 @@ public class CallModel {
 
     func isCurrent() -> Bool {
         return self.state == .current || self.state == .hold ||
-            self.state == .unhold || self.state == .ringing
+            self.state == .resume || self.state == .ringing
     }
 
     func updateParticipantsCallId(callId: String) {
