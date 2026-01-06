3761fea27e258ae43524f0c5277b9e8f40ee602c
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2026-01-06 14:46:11 -0500

    app: fix UIScene lifecycle
    Implement UIScene-based lifecycle management
    as required by Apple.
    
    Change-Id: I965a50d4a2203e954693951319fe6b82ab1f95c9

--- Ring/Ring/AppDelegate.swift
+++ Ring/Ring/AppDelegate.swift
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2025 Savoir-faire Linux Inc.
+ * Copyright (C) 2017-2026 Savoir-faire Linux Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@ import ContactsUI
 import os
 
 // swiftlint:disable identifier_name type_body_length
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     var window: UIWindow?
     let dBManager = DBManager(profileHepler: ProfileDataHelper(),
@@ -97,6 +97,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return AppCoordinator(injectionBag: self.injectionBag)
     }()
 
+    // MARK: - Public Interface for SceneDelegate
+    var rootViewController: UIViewController {
+        return appCoordinator.rootViewController
+    }
+
+    func startAppCoordinator() {
+        appCoordinator.start()
+    }
+
     private let log = SwiftyBeaver.self
 
     private let disposeBag = DisposeBag()
@@ -105,6 +114,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     private static let shouldHandleNotification = NSNotification.Name("com.savoirfairelinux.jami.shouldHandleNotification")
     private let backgrounTaskQueue = DispatchQueue(label: "backgrounTaskQueue")
 
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfig.delegateClass = SceneDelegate.self
+        return sceneConfig
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // ignore sigpipe
@@ -112,9 +130,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let SIG_IGN = unsafeBitCast(OpaquePointer(bitPattern: 1), to: SigHandler.self)
         signal(SIGPIPE, SIG_IGN)
         // swiftlint:enable nesting
-
-        self.window = UIWindow()
-
         UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
         if UserDefaults.standard.value(forKey: automaticDownloadFilesKey) == nil {
             UserDefaults.standard.set(true, forKey: automaticDownloadFilesKey)
@@ -129,16 +144,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         log.removeAllDestinations()
         #endif
 
-        // move files from the app container to the group container, so it could be accessed by notification extension
-        if !self.moveDataToGroupContainer() {
-            self.window?.rootViewController = self.appCoordinator.rootViewController
-            self.window?.makeKeyAndVisible()
-            let alertController = UIAlertController(title: "Unable to start Jami", message: "An error occurred while attempting to start Jami. Please try again.", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
-            alertController.addAction(okAction)
-            self.window?.rootViewController?.present(alertController, animated: true, completion: nil)
-            return true
-        }
         PreferenceManager.registerDonationsDefaults()
 
         self.addListenerForNotification()
@@ -181,8 +186,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                                                         callsProvider: self.callsProvider, requestsService: self.requestsService, profileService: self.profileService,
                                                         presenceService: self.presenceService)
         self.videoManager = VideoManager(with: self.callService, videoService: self.videoService)
-        self.window?.rootViewController = self.appCoordinator.rootViewController
-        self.window?.makeKeyAndVisible()
 
         prepareVideoAcceleration()
         prepareAccounts()
@@ -199,46 +202,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         if let path = self.certificatePath() {
             setenv("CA_ROOT_FILE", path, 1)
         }
-        self.window?.backgroundColor = UIColor.systemBackground
         return true
-    }
-
-    func moveDataToGroupContainer() -> Bool {
-        let usingGroupConatinerKey = "usingGroupConatiner"
-        if UserDefaults.standard.bool(forKey: usingGroupConatinerKey) {
-            return true
-        }
-        guard let groupDocUrl = Constants.documentsPath,
-              let groupCachesUrl = Constants.cachesPath,
-              let appDocURL = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false),
-              let appLibrURL = try? FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: false) else {
-            return false
-        }
-        if FileManager.default.fileExists(atPath: groupDocUrl.path) {
-            try? FileManager.default.removeItem(atPath: groupDocUrl.path)
-        }
-        if FileManager.default.fileExists(atPath: groupCachesUrl.path) {
-            try? FileManager.default.removeItem(atPath: groupCachesUrl.path)
-        }
-        let appCacheDir = appLibrURL.appendingPathComponent("Caches")
-        do {
-            try FileManager.default.copyItem(at: appDocURL, to: groupDocUrl)
-            try FileManager.default.copyItem(at: appCacheDir, to: groupCachesUrl)
-        } catch {
-            print(error.localizedDescription)
-            try? FileManager.default.removeItem(atPath: groupDocUrl.path)
-            try? FileManager.default.removeItem(atPath: groupCachesUrl.path)
-            return false
-        }
-        if let fileURLs = try? FileManager.default.contentsOfDirectory(at: appDocURL,
-                                                                       includingPropertiesForKeys: nil,
-                                                                       options: .skipsHiddenFiles) {
-            for fileURL in fileURLs {
-                try? FileManager.default.removeItem(at: fileURL)
-            }
-        }
-        UserDefaults.standard.setValue(true, forKey: usingGroupConatinerKey)
-        return UserDefaults.standard.bool(forKey: usingGroupConatinerKey)
     }
 
     func addListenerForNotification() {
@@ -286,8 +250,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             .disposed(by: self.disposeBag)
         self.accountService.initialAccountsLoading()
             .subscribe(onCompleted: {
+                DispatchQueue.main.async {
+                    self.appCoordinator.initialLoadingCompleted()
+                }
                 // set selected account if exists
-                self.appCoordinator.start()
                 if !self.accountService.hasAccounts() {
                     // Set default download transfer limit to 20MB.
                     let userDefaults = UserDefaults.standard
@@ -369,34 +335,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         self.presenceService.subscribeBuddies(withAccount: account.id, withContacts: self.contactsService.contacts.value, subscribe: true)
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        self.log.warning("entering background")
+    // MARK: - Scene Lifecycle Methods
+
+    func sceneDidEnterBackground() {
         guard let account = self.accountService.currentAccount else { return }
         self.presenceService.subscribeBuddies(withAccount: account.id, withContacts: self.contactsService.contacts.value, subscribe: false)
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        self.log.warning("entering foreground")
+    func sceneWillEnterForeground() {
         self.updateNotificationAvailability()
         guard let account = self.accountService.currentAccount else { return }
         self.presenceService.subscribeBuddies(withAccount: account.id, withContacts: self.contactsService.contacts.value, subscribe: true)
+    }
+
+    func sceneDidBecomeActive() {
+        self.clearBadgeNumber()
+        guard let account = self.accountService.currentAccount else { return }
+        self.presenceService.subscribeBuddies(withAccount: account.id, withContacts: self.contactsService.contacts.value, subscribe: true)
+    }
+
+    func sceneWillResignActive() {
+        guard let account = self.accountService.currentAccount else { return }
+        self.presenceService.subscribeBuddies(withAccount: account.id, withContacts: self.contactsService.contacts.value, subscribe: false)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         self.callsProvider.stopAllUnhandeledCalls()
         self.cleanTestDataIfNeed()
         self.stopDaemon()
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        self.clearBadgeNumber()
-        guard let account = self.accountService.currentAccount else { return }
-        self.presenceService.subscribeBuddies(withAccount: account.id, withContacts: self.contactsService.contacts.value, subscribe: true)
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        guard let account = self.accountService.currentAccount else { return }
-        self.presenceService.subscribeBuddies(withAccount: account.id, withContacts: self.contactsService.contacts.value, subscribe: false)
     }
 
     func prepareVideoAcceleration() {
@@ -585,6 +551,9 @@ extension AppDelegate {
     }
 
     func findContactAndStartCall(hash: String, isVideo: Bool) {
+        if callsProvider.hasActiveCalls() {
+            return
+        }
         // if saved Jami hash
         if hash.isSHA1() {
             let contactUri = JamiURI(schema: URIType.ring, infoHash: hash)
@@ -641,26 +610,6 @@ extension AppDelegate {
                 })
                 .disposed(by: self.disposeBag)
         }
-    }
-
-    func application(_ application: UIApplication,
-                     continue userActivity: NSUserActivity,
-                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        if self.accountService.boothMode() {
-            return false
-        }
-        /*
-         This method could be called when activating camera from CallKit.
-         In this case we will have existing call with CallKit.
-         Othervise it was called from Contacts app.
-         We need find contact and start a call
-         */
-        if self.callsProvider.hasActiveCalls() { return false }
-        guard let handle = userActivity.startCallHandle else {
-            return false
-        }
-        self.findContactAndStartCall(hash: handle.hash, isVideo: handle.isVideo)
-        return true
     }
 }
 
