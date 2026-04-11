32800d3131ec91feecc0f09d69179ff2e6d059a3
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2026-04-10 20:09:51 -0400

    assets: consolidate color assets and drop unused ones
    Change-Id: Idef83aa442fc1c4ec7d538a94bfb7dc99b3c72ea

--- Ring/Ring/Constants/Generated/Images.swift
+++ Ring/Ring/Constants/Generated/Images.swift
@@ -13,8 +13,6 @@
 #endif
 
 // Deprecated typealiases
-@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
-internal typealias AssetColorTypeAlias = ColorAsset.Color
 @available(*, deprecated, renamed: "ImageAsset.Image", message: "This typealias will be removed in SwiftGen 7.0")
 internal typealias AssetImageTypeAlias = ImageAsset.Image
 
@@ -25,90 +23,18 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 internal enum Asset {
   internal static let addPeopleInSwarm = ImageAsset(name: "addPeopleInSwarm")
-  internal static let backgroundInputText = ColorAsset(name: "background_input_text")
   internal static let backgroundLogin = ImageAsset(name: "background_login")
-  internal static let backgroundMsgReceived = ColorAsset(name: "background_msg_received")
   internal static let editSwarmImage = ImageAsset(name: "editSwarmImage")
   internal static let jamiIcon = ImageAsset(name: "jamiIcon")
   internal static let jamiLogo = ImageAsset(name: "jamiLogo")
   internal static let jamiGnupackage = ImageAsset(name: "jami_gnupackage")
-  internal static let messageBackgroundColor = ColorAsset(name: "message_background_color")
   internal static let messageSentIndicator = ImageAsset(name: "message_sent_indicator")
   internal static let phoneBook = ImageAsset(name: "phone_book")
-  internal static let rowSelected = ColorAsset(name: "row_selected")
   internal static let scan = ImageAsset(name: "scan")
-  internal static let shadowColor = ColorAsset(name: "shadow_color")
-  internal static let textBlueColor = ColorAsset(name: "text_blue_color")
-  internal static let textFieldBackgroundColor = ColorAsset(name: "text_field_background_color")
-  internal static let textSecondaryColor = ColorAsset(name: "text_secondary_color")
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
-
-internal final class ColorAsset {
-  internal fileprivate(set) var name: String
-
-  #if os(macOS)
-  internal typealias Color = NSColor
-  #elseif os(iOS) || os(tvOS) || os(watchOS)
-  internal typealias Color = UIColor
-  #endif
-
-  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
-  internal private(set) lazy var color: Color = {
-    guard let color = Color(asset: self) else {
-      fatalError("Unable to load color asset named \(name).")
-    }
-    return color
-  }()
-
-  #if os(iOS) || os(tvOS)
-  @available(iOS 11.0, tvOS 11.0, *)
-  internal func color(compatibleWith traitCollection: UITraitCollection) -> Color {
-    let bundle = BundleToken.bundle
-    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
-      fatalError("Unable to load color asset named \(name).")
-    }
-    return color
-  }
-  #endif
-
-  #if canImport(SwiftUI)
-  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
-  internal private(set) lazy var swiftUIColor: SwiftUI.Color = {
-    SwiftUI.Color(asset: self)
-  }()
-  #endif
-
-  fileprivate init(name: String) {
-    self.name = name
-  }
-}
-
-internal extension ColorAsset.Color {
-  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
-  convenience init?(asset: ColorAsset) {
-    let bundle = BundleToken.bundle
-    #if os(iOS) || os(tvOS)
-    self.init(named: asset.name, in: bundle, compatibleWith: nil)
-    #elseif os(macOS)
-    self.init(named: NSColor.Name(asset.name), bundle: bundle)
-    #elseif os(watchOS)
-    self.init(named: asset.name)
-    #endif
-  }
-}
-
-#if canImport(SwiftUI)
-@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
-internal extension SwiftUI.Color {
-  init(asset: ColorAsset) {
-    let bundle = BundleToken.bundle
-    self.init(asset.name, bundle: bundle)
-  }
-}
-#endif
 
 internal struct ImageAsset {
   internal fileprivate(set) var name: String
